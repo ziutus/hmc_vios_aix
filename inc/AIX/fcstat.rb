722f13162100cf581fed895a17a2929252258b1b
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2017-09-17 20:43:19 +0200

    New class with test file to analyze output from fcstat command.

---
+++ inc/AIX/fcstat.rb
@@ -0,0 +1,136 @@
+class Fcstat
+
+  attr_accessor :data_string_raw, :device, :data
+
+  def initialize(string)
+
+    @data = Hash.new()
+    @data_string_raw=''
+
+    if string.length > 0
+      @data_string_raw = string
+      self.parse(string)
+    end
+  end
+
+  def get_stats()
+    @data
+  end
+
+  def parse(string)
+
+    regexp = %r{FIBRE\sCHANNEL\sSTATISTICS\sREPORT:\s(fcs\d+)\s+
+Device\sType:\s(.*?)\s+
+Serial\sNumber:\s(\w+)\s
+Option\sROM\sVersion:\s(\w+)\s
+ZA:\s([\w\.]+)\s
+Node\sWWN:\s(\w+)\s
+Port\sWWN:\s(\w+)\s
+\s
+FC4\sTypes\s
+\s+Supported:\s+(0x\d+)\s
+\s+Active:\s+(0x\d+)\s
+Class\sof\sService:\s+(\d+)\s
+Port\sFC\sID:\s+(\w+)\s
+Port\sSpeed\s\(supported\):\s+(\d+\sGBIT)\s
+Port\sSpeed\s\(running\):\s+(\d+\sGBIT)\s
+Port\sType:\s(Fabric)\s
+\s
+Seconds\sSince\sLast\sReset:\s+(\d+)\s
+\s
+Transmit\sStatistics\s+Receive\sStatistics\s
+[-]+\s+[-]+\s
+Frames:\s(\d+)\s+Frames:\s(\d+)\s+
+Words:\s(\d+)\s+Words:\s(\d+)\s+
+\s
+LIP\sCount:\s+(\d+)\s
+NOS\sCount:\s+(\d+)\s
+Error\sFrames:\s+(\d+)\s
+Dumped\sFrames:\s+(\d+)\s
+Link\sFailure\sCount:\s+(\d+)\s
+Loss\sof\sSync\sCount:\s+(\d+)\s
+Loss\sof\sSignal:\s+(\d+)\s
+Primitive\sSeq\sProtocol\sErr\sCount:\s+(\d+)\s
+Invalid\sTx\sWord\sCount:\s+(\d+)\s
+Invalid\sCRC\sCount:\s+(\d+)\s
+\s+
+IP\sover\sFC\sAdapter\sDriver\sInformation\s
+\s+No\sDMA\sResource\sCount:\s+(\d+)\s
+\s+No\sAdapter\sElements\sCount:\s+(\d+)\s
+\s
+FC\sSCSI\sAdapter\sDriver\sInformation\s
+\s+No\sDMA\sResource\sCount:\s+(\d+)\s
+\s+No\sAdapter\sElements\sCount:\s+(\d+)\s
+\s+No\sCommand\sResource\sCount:\s+(\d+)\s
+\s
+IP\sover\sFC\sTraffic\sStatistics\s
+\s+Input\sRequests:\s+(\d+)\s
+\s+Output\sRequests:\s+(\d+)\s
+\s+Control\sRequests:\s+(\d+)\s
+\s+Input\sBytes:\s+(\d+)\s
+\s+Output\sBytes:\s+(\d+)\s
+\s
+FC\sSCSI\sTraffic\sStatistics\s
+\s+Input\sRequests:\s+(\d+)\s
+\s+Output\sRequests:\s+(\d+)\s
+\s+Control\sRequests:\s+(\d+)\s
+\s+Input\sBytes:\s+(\d+)\s
+\s+Output\sBytes:\s+(\d+)\s*
+}x
+
+    match = regexp.match(string)
+
+    if match
+      @device         = match[1]
+
+      @data['device']             = match[1]
+      @data['Device Type']        = match[2]
+      @data['Serial Number']      = match[3]
+      @data['Option ROM Version'] = match[4]
+      @data['ZA']                 = match[5]
+      @data['Node WWN']           = match[6]
+      @data['Port WWN']           = match[7]
+      @data['FC4 Types'] = { 'Supported' => match[8], 'Active' => match[9]  }
+
+      @data['Class of Service']       = match[10]
+      @data['Port FC ID']         = match[11]
+      @data['Port Speed (supported)'] = match[12]
+      @data['Port Speed (running)']   = match[13]
+      @data['Port Type']              = match[14]
+      @data['Seconds Since Last Reset'] = match[15].to_i
+      @data['Transmit Statistics'] = { 'Frames' => match[16].to_i, 'Words'  => match[18].to_i}
+      @data['Receive Statistics']= { 'Frames' => match[17].to_i, 'Words'  => match[19].to_i}
+      @data['LIP Count']          = match[20].to_i
+      @data['NOS Count']          = match[21].to_i
+      @data['Error Frames']       = match[22].to_i
+      @data['Dumped Frames']      = match[23].to_i
+      @data['Link Failure Count'] = match[24].to_i
+      @data['Loss of Sync Count'] = match[25].to_i
+      @data['Loss of Signal']     = match[26].to_i
+      @data['Primitive Seq Protocol Err Count'] = match[27].to_i
+      @data['Invalid Tx Word Count'] = match[28].to_i
+      @data['Invalid CRC Count']     = match[29].to_i
+      @data['IP over FC Adapter Driver Information'] = { 'No DMA Resource Count'=> match[30].to_i,  'No Adapter Elements Count' => match[31].to_i  }
+
+      @data['FC SCSI Adapter Driver Information'] = { 'No DMA Resource Count' => match[32].to_i, 'No Adapter Elements Count' => match[33].to_i, 'No Command Resource Count' => match[34].to_i }
+
+      @data['IP over FC Traffic Statistics']= { 'Input Requests'  => match[35].to_i, 'Output Requests' => match[36].to_i,  'Control Requests' => match[37].to_i, 'Input Bytes' => match[38].to_i,  'Output Bytes' => match[39].to_i  }
+      @data['FC SCSI Traffic Statistics'] =   { 'Input Requests'  => match[40].to_i, 'Output Requests' => match[41].to_i,'Control Requests' => match[42].to_i, 'Input Bytes' => match[43].to_i,  'Output Bytes' => match[44].to_i  }
+
+    else
+      puts "can't analyze string, regexp is not working"
+      puts string
+      puts regexp
+      puts match
+      puts "regexp couldn't decode string #{string}"
+      raise
+    end
+  end
+
+
+
+
+
+
+
+end
---
+++ t/aix_fcstat.rb
@@ -0,0 +1,143 @@
+$LOAD_PATH << File.dirname(__FILE__)+"/../inc"
+
+require "test/unit"
+require 'pp'
+require 'AIX/fcstat'
+
+
+
+class TestAixFcstat < Test::Unit::TestCase
+
+  def test_Fcstat1
+
+    # example output from fcstat command is taken from IBM website:
+    # https://www.ibm.com/support/knowledgecenter/en/ssw_aix_61/com.ibm.aix.cmds2/fcstat.htm
+    # some data were changed to next numbers to be sure that regexp is working correctly
+    string = 'FIBRE CHANNEL STATISTICS REPORT: fcs0
+
+Device Type: FC Adapter (df1000f9)
+Serial Number: 1E313BB001
+Option ROM Version: 02C82115
+ZA: B1F2.10A5
+Node WWN: 20000000C9487B04
+Port WWN: 10000000C9416DA4
+
+FC4 Types
+  Supported: 0x0000010000000000000000000000000000000000000000000000000000000000
+  Active:    0x0000010000000000000000000000000000000000000000000000000000000000
+Class of Service: 4
+Port FC ID: 011400
+Port Speed (supported): 2 GBIT
+Port Speed (running):   1 GBIT
+Port Type: Fabric
+
+Seconds Since Last Reset: 345422
+
+Transmit Statistics    Receive Statistics
+-------------------    ------------------
+Frames: 1              Frames: 2
+Words: 3               Words: 4
+
+LIP Count: 5
+NOS Count: 6
+Error Frames:  7
+Dumped Frames: 8
+Link Failure Count: 9
+Loss of Sync Count: 10
+Loss of Signal: 11
+Primitive Seq Protocol Err Count: 12
+Invalid Tx Word Count: 13
+Invalid CRC Count: 14
+
+IP over FC Adapter Driver Information
+  No DMA Resource Count: 15
+  No Adapter Elements Count: 16
+
+FC SCSI Adapter Driver Information
+  No DMA Resource Count: 17
+  No Adapter Elements Count: 18
+  No Command Resource Count: 19
+
+IP over FC Traffic Statistics
+  Input Requests:   20
+  Output Requests:  21
+  Control Requests: 22
+  Input Bytes:  23
+  Output Bytes: 24
+
+FC SCSI Traffic Statistics
+  Input Requests:   16289
+  Output Requests:  48930
+  Control Requests: 11791
+  Input Bytes:  128349517
+  Output Bytes: 209883136';
+
+    fcstat = Fcstat.new(string)
+    data = fcstat.get_stats()
+
+    assert_equal("fcs0",     fcstat.device,     "fcstat device name")
+    assert_equal("fcs0",     data["device"],     "fcstat device name")
+
+    assert_equal("fcs0",     fcstat.data["device"],     "fcstat device name")
+
+    assert_equal('FC Adapter (df1000f9)', fcstat.data["Device Type"])
+    assert_equal('1E313BB001',            fcstat.data["Serial Number"])
+    assert_equal('02C82115', fcstat.data['Option ROM Version'])
+    assert_equal( 'B1F2.10A5',fcstat.data['ZA'])
+
+    assert_equal( '20000000C9487B04', fcstat.data['Node WWN'])
+	  assert_equal( '10000000C9416DA4', fcstat.data['Port WWN'])
+
+    assert_equal('0x0000010000000000000000000000000000000000000000000000000000000000',     fcstat.data["FC4 Types"]['Supported'],     "fcstat device name")
+    assert_equal('0x0000010000000000000000000000000000000000000000000000000000000000',     fcstat.data["FC4 Types"]['Active'],     "fcstat device name")
+
+    assert_equal('4', fcstat.data['Class of Service'])
+
+    assert_equal('011400', fcstat.data['Port FC ID'])
+    assert_equal('2 GBIT', fcstat.data['Port Speed (supported)'])
+    assert_equal('1 GBIT', fcstat.data['Port Speed (running)'])
+    assert_equal('Fabric', fcstat.data['Port Type'])
+    assert_equal(345422,   fcstat.data['Seconds Since Last Reset'])
+
+    assert_equal(1, fcstat.data['Transmit Statistics']['Frames'])
+    assert_equal(3, fcstat.data['Transmit Statistics']['Words'])
+    assert_equal(2, fcstat.data['Receive Statistics']['Frames'])
+    assert_equal(4, fcstat.data['Receive Statistics']['Words'])
+
+    assert_equal(5  , fcstat.data['LIP Count'])
+    assert_equal(6  , fcstat.data['NOS Count'])
+    assert_equal(7  , fcstat.data['Error Frames'])
+    assert_equal(8  , fcstat.data['Dumped Frames'])
+    assert_equal(9  , fcstat.data['Link Failure Count'])
+    assert_equal(10 , fcstat.data['Loss of Sync Count'])
+    assert_equal(11 , fcstat.data['Loss of Signal'])
+    assert_equal(12 , fcstat.data['Primitive Seq Protocol Err Count'])
+    assert_equal(13 , fcstat.data['Invalid Tx Word Count'])
+    assert_equal(14 , fcstat.data['Invalid CRC Count'])
+    assert_equal(15 , fcstat.data['IP over FC Adapter Driver Information']['No DMA Resource Count'])
+    assert_equal(16 , fcstat.data['IP over FC Adapter Driver Information']['No Adapter Elements Count'])
+
+    assert_equal(17 , fcstat.data['FC SCSI Adapter Driver Information']['No DMA Resource Count'])
+    assert_equal(18 , fcstat.data['FC SCSI Adapter Driver Information']['No Adapter Elements Count'])
+    assert_equal(19 , fcstat.data['FC SCSI Adapter Driver Information']['No Command Resource Count'])
+
+
+
+    assert_equal(20 , fcstat.data['IP over FC Traffic Statistics']['Input Requests'])
+    assert_equal(21 , fcstat.data['IP over FC Traffic Statistics']['Output Requests'])
+    assert_equal(22 , fcstat.data['IP over FC Traffic Statistics']['Control Requests'])
+    assert_equal(23 , fcstat.data['IP over FC Traffic Statistics']['Input Bytes'])
+    assert_equal(24 , fcstat.data['IP over FC Traffic Statistics']['Output Bytes'])
+
+    assert_equal(16289, fcstat.data['FC SCSI Traffic Statistics']['Input Requests'])
+    assert_equal(48930, fcstat.data['FC SCSI Traffic Statistics']['Output Requests'])
+    assert_equal(11791, fcstat.data['FC SCSI Traffic Statistics']['Control Requests'])
+    assert_equal(128349517, fcstat.data['FC SCSI Traffic Statistics']['Input Bytes'])
+    assert_equal(209883136, fcstat.data['FC SCSI Traffic Statistics']['Output Bytes'])
+
+
+
+
+
+  end
+end
