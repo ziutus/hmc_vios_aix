226eff060011f2be2cf141975613d3ff03710e5d
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2017-09-26 01:00:58 +0200

    Added support for Virtual FC adapters

---
+++ inc/HMC/VirtualFCAdapter.rb
@@ -0,0 +1,78 @@
+class VirtualFCAdapter
+
+	#see: http://www-01.ibm.com/support/docview.wss?uid=nas8N1011009
+	#see: https://www.ibm.com/support/knowledgecenter/en/POWER8/p8edm/mksyscfg.html
+	
+	attr_accessor :virtualSlotNumber, :clientOrServer, :remoteLparID, :remoteLparName, :remoteSlotNumber 
+	attr_accessor :wwpn1, :wwpn2
+	attr_accessor :isRequired 
+
+	def initialize string='' 
+		@virtualSlotNumber 
+		@clientOrServer
+		@remoteLparID
+		@remoteLparName
+		@remoteSlotNumber 
+		@wwpn1
+		@wwpn2
+		@isRequired=0
+		
+		if string.length > 0
+		  @data_string_raw = string
+		  self.parse(string)
+		end			
+	end
+		
+		
+	def validate
+		raise "virtualSlotNumber not defined" unless (@virtualSlotNumber.is_a? Numeric)
+	end
+		
+	def to_s
+	
+		self.validate()
+
+		#virtual-slot-number/client-or-server/[remote-lpar-ID]/[remote-lpar-name]/remote-slot-number/[wwpns]/is-required
+
+		
+		result ="#{@virtualSlotNumber}/#{clientOrServer}/#{@remoteLparID}/#{@remoteLparName}/#{@remoteSlotNumber}/" 
+		result = result+"#{@wwpn1}" 	   unless (@wwpn1.nil?)
+		result = result+",#{@wwpn2}" 	   unless (@wwpn2.nil?)
+		result = result+"/#{@isRequired}"
+		
+		result
+	end
+	
+	def decode string
+
+		#virtual-slot-number/client-or-server/[remote-lpar-ID]/[remote-lpar-name]/remote-slot-number/[wwpns]/is-required
+	
+		regExp =  %r{(\d+)/(server|client)/(\d+)/([\w\-\_]+|)/(\d+)/(\w{16}\,\w{16}|\w{16}|)/(0|1)}
+
+		match = regExp.match(string)
+		
+		raise "RegExp couldn't decode string #{string}" unless match
+
+		@virtualSlotNumber 	= match[1].to_i	
+		@clientOrServer 	= match[2]	
+		@remoteLparID 		= match[3].to_i	
+		@remoteLparName		= match[4]
+		@remoteSlotNumber 	= match[5].to_i	
+		wwpns				= match[6]
+		@isRequired			= match[7].to_i
+		
+		if match2 = %r{^\s*(\w{16})\,(\w{16})\s*$}.match(wwpns) 
+			@wwpn1 = match2[1]
+			@wwpn2 = match2[2]
+		elsif  match3 = %r{^\s*(\w{16})\s*$}.match(wwpns) 
+			@wwpn1 = match3[1]
+			@wwpn2 = nil 
+		end
+
+		
+	end
+
+	alias :parse :decode
+
+	
+end
---
+++ t/hmc_VirtualFCAdapter.rb
@@ -0,0 +1,55 @@
+$LOAD_PATH << File.dirname(__FILE__)+"./inc"
+$LOAD_PATH << File.dirname(__FILE__)
+
+require 'HMC/VirtualFCAdapter'
+require "test/unit"
+
+
+class TestVirtualFCAdapter < Test::Unit::TestCase
+ 
+	#example data: http://www-01.ibm.com/support/docview.wss?uid=nas8N1011009
+	def test_base
+		adapter = VirtualFCAdapter.new()
+		
+		adapter.virtualSlotNumber=10
+		adapter.clientOrServer="client"
+		adapter.remoteLparID=20
+		adapter.remoteLparName="VIOS1-Dilling"
+		adapter.remoteSlotNumber=34 
+		adapter.wwpn1="c0507602f9ac000a"
+		adapter.wwpn2="c0507602f9ac000b"
+		adapter.isRequired=1
+				
+		assert_equal("10/client/20/VIOS1-Dilling/34/c0507602f9ac000a,c0507602f9ac000b/1", adapter.to_s() )
+	end
+
+	#example data: http://www-01.ibm.com/support/docview.wss?uid=nas8N1011009	
+	def test_decode
+	
+		adapter = VirtualFCAdapter.new()
+		adapter.decode("10/client/20/VIOS1-Dilling/34/c0507602f9ac000a,c0507602f9ac000b/1")
+
+		#virtual-slot-number/client-or-server/[remote-lpar-ID]/[remote-lpar-name]/remote-slot-number/[wwpns]/is-required	
+
+		assert_equal(10,		adapter.virtualSlotNumber)
+		assert_equal("client", 	adapter.clientOrServer)
+		assert_equal(20, 		adapter.remoteLparID)
+		assert_equal(34, 		adapter.remoteSlotNumber) 
+		assert_equal(1,			adapter.isRequired)
+
+		assert_equal("VIOS1-Dilling", 	 adapter.remoteLparName)
+		assert_equal("c0507602f9ac000a", adapter.wwpn1)
+		assert_equal("c0507602f9ac000b", adapter.wwpn2)
+		
+		
+	end	
+	
+	# def test_validation 
+
+		# adapter2 = VirtualFcAdapter.new()
+	
+		# exception = assert_raise(RuntimeError) {adapter2.to_s()}
+		# assert_equal("virtualSlotNumber not defined", exception.message)
+	
+	# end
+end
