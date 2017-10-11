bd74969bcb895c040aeecf38b2cefa05abae713b
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2017-10-11 20:20:08 +0200

    Profiles adding and removing updated.

--- inc/HMC/HmcLpar.rb
+++ inc/HMC/HmcLpar.rb
@@ -1,19 +1,28 @@
-require "pp"
-
 class HmcLpar
 
-	attr_accessor :sys, :name, :lpar_name
+	attr_accessor :sys
+  attr_accessor :name
+  attr_accessor :lpar_name
 	attr_accessor :hmcs
+
 	attr_reader :profiles
 
-	def initialize lpar_id
+	def initialize(lpar_id)
 	
 		@lpar_id  = lpar_id
-    @profiles = []
+    @profiles = Hash.new
 	end
 
-  def add_profile profile
-    @profiles.push(profile)
+  def profile_add(profile)
+    @profiles[profile.name]= profile
+  end
+
+	def profile_delete(profile_name)
+		@profiles.delete(profile_name)
+	end
+
+  def profile_exist?(profile_name)
+    @profiles.include?(profile_name)
   end
 
 end
