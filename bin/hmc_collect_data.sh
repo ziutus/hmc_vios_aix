efb6dc904d114eee1c12ae224e545c94d8ec096c
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2017-10-15 20:22:35 +0200

    Small refactoring.

--- bin/hmc_collect_data.sh
+++ bin/hmc_collect_data.sh
@@ -3,16 +3,16 @@ set -x
 
 MYNAME=$(basename $0)
 DATE=$(date +"%Y%m%d_%H%M")
-DIR_BASE="/var/opt/unix4you/data/hmc"
-PASSWORD="zuuz00zuuz"
+DIR_BASE='/var/opt/unix4you/data/hmc'
+PASSWORD='XXX'
 
 #HMCS=$(cat conifg/hmcs.txt)
-HMCS="192.168.200.33"
-HMC_USER="hscroot"
-REPORT="frames_profiles"
-SSH="/usr/bin/ssh "
+HMCS='192.168.200.33'
+HMC_USER='hscroot'
+REPORT='frames_profiles'
+SSH='/usr/bin/ssh '
 
-[ $PASSWORD == "XXX" ] && echo "Default password has been not changed, please update it. Exiting..." && exit 2
+[ $PASSWORD == 'XXX' ] && echo 'Default password has been not changed, please update it. Exiting...' && exit 2
 [ ! -d $DIR_BASE ] &&  echo "The data dir $DIR_BASE doesn't exist, did you install correctly package? Exiting..." && exit 3
 
 #TODO: add check if directory can be written 
@@ -26,10 +26,10 @@ do
 	#TODO: Add check if used is key and is possible to connect by ssh
 	
 	
-	if [ $REPORT == "frames_profiles" ] 
+	if [ $REPORT == 'frames_profiles' ]
 	then
 
-		$SSH $HMC_USER@$HMC "lssyscfg -r sys -F name,serial_num,state" > $DIR/lssyscfg_r_sys_F_name_serial_num_state.txt
+		$SSH $HMC_USER@$HMC 'lssyscfg -r sys -F name,serial_num,state' > $DIR/lssyscfg_r_sys_F_name_serial_num_state.txt
 
 		for FRAME in `cat $DIR/lssyscfg_r_sys_F_name_serial_num_state.txt | grep -v "\." | cut -d "," -f 1`; 
 		do 
@@ -38,10 +38,10 @@ do
 		done 	
 	fi
 
-	if [ $REPORT == "ALL" ] 
+	if [ $REPORT == 'ALL' ]
 	then
 
-		$SSH $HMC_USER@$HMC "lssyscfg -r sys -F name,serial_num,state" > $DIR/lssyscfg_r_sys_F_name_serial_num_state.txt
+		$SSH $HMC_USER@$HMC 'lssyscfg -r sys -F name,serial_num,state' > $DIR/lssyscfg_r_sys_F_name_serial_num_state.txt
 
 		for FRAME in `cat $DIR/lssyscfg_r_sys_F_name_serial_num_state.txt | grep -v "\." | cut -d "," -f 1`; 
 		do 
