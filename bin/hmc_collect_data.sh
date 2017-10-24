436c4f8a5926134948e95d6351823a10d0d44ec0
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2017-10-24 21:16:12 +0200

    The script is able now to read HMCs list from file or command line.

--- bin/hmc_collect_data.sh
+++ bin/hmc_collect_data.sh
@@ -8,16 +8,67 @@ DATE=$(date +"%Y%m%d_%H%M")
 DIR_BASE='/var/opt/unix4you/data/hmc'
 PASSWORD=''
 
-
 #HMCS=$(cat conifg/hmcs.txt)
-HMCS='192.168.200.33'
+HMC=''
 HMC_USER='hscroot'
 REPORT='ALL'
+HMC_FILE=''
+HMC_STRING=''
+
+function usage {
+
+    echo STDERR "Usage of $MYNAME
+        $MYNAME [-h|--help] [-H|--hmcs HMC1[,HMC2[,HMC3...]]] [-f|--file FILE_LIST ] [-r|--report REPORT_TYPE] [-u|--user HMC_USER ]
+
+          -H|--hmcs HMC1[,HMC2[,HMC3...]]] - names of HMCs from which data should be collected
+          -f|--file FILENAME               - filename with list of HMCs from which data should be collected
+          -u|--user HMC_USER               - name of HMC_USER on which data should be collected (default: $HMC_USER)
+          -r|--report REPORT_NAME          - name of report  for which data will be collected (default: $REPORT)
+          -h|--help                        - show this message
+    "
+
+    exit 2
+}
+
+
+while [ $# -gt 0 ];
+do
+    case $1 in
+    -h|--help)   shift;              usage;;
+    -H|--hmcs)   shift; HMC_STRING=$1; shift;;
+    -f|--file)   shift; HMC_FILE=$1;   shift;;
+    -r|--report) shift; REPORT=$1;     shift;;
+    -u|--user)   shift; HMC_USER=$1;   shift;;
+    -p|--password|--passwd) shift; PASSWORD=$1; shift;;
+
+    *) echo "Wrong option $1"; exit 1;
+    esac
+done
+
+
+if [ ! -z $HMC_FILE ]
+then
+
+    for HMC in `cat $HMC_FILE`
+    do
+        INDEX=${#HMCS[*]}
+        HMCS[$INDEX]=$HMC
+    done
+fi
+
+if [ ! -z $HMC_STRING ]
+then
+    for HMC in `echo $HMC_STRING | tr ',' "\n"`
+    do
+        INDEX=${#HMCS[*]}
+        HMCS[$INDEX]=$HMC
+    done
+fi
 
 HMC_OK=0
 HMC_ERRORS=0
 
-password_write_to_file_if_empty
+#password_write_to_file_if_empty
 
 
 if [ -x /opt/tectia/bin/sshg3 ]
@@ -27,22 +78,32 @@ else
     SSH="/usr/bin/ssh -l ${HMC_USER} "
 fi
 
-
 [ "${PASSWORD}" == 'XXX' ] && echo 'Default password has been not changed, please update it. Exiting...' && exit 2
-[ ! -d ${DIR_BASE} ]       && echo "The data dir $DIR_BASE doesn't exist, did you install correctly package? Exiting..." && exit 3
+#[ ! -d ${DIR_BASE} ]       && echo "The data dir $DIR_BASE doesn't exist, did you install correctly package? Exiting..." && exit 3
 
 #TODO: add check if directory can be written 
 
-for HMC in ${HMCS}
+for HMC in ${HMCS[*]}
+
 do
 	echo ">>Working on HMC: ${HMC}"
 
-	#TODO: Add check if used is key and is possible to connect by ssh
-	${SSH} ${HMC} 'uname -n' 
+    #ping test
+    ping -c4  $HMC > /dev/null
+    RC=$?
+    if [ $RC -ne 0 ]
+    then
+		echo "Can't ping HMC ${HMC}"
+        ((HMC_ERRORS=$HMC_ERRORS+1))
+		continue
+    fi
+
+    #ssh connection test (without asking about password)
+	${SSH} ${HMC} 'uname -n'
 	RC=$?
 	if [ $RC -ne 0 ]
 	then
-		echo "Can't connect to HMC ${HMC}" 
+		echo "Can't connect by SSH to HMC ${HMC}"
         ((HMC_ERRORS=$HMC_ERRORS+1))
 		continue
 	fi
@@ -61,6 +122,18 @@ do
 		${SSH} ${HMC} 'lshmcusr' > ${DIR}/lshmcusr.txt
     fi
 
+
+    if [ ${REPORT} == 'HMC_resouceroles' ]
+    then
+        ${SSH} ${HMC} 'lsaccfg  -t resourcerole' > ${DIR}/resourceroles.txt
+		${SSH} ${HMC} 'lssyscfg -r sys -F name,type_model,serial_num,state' > ${DIR}/lssyscfg_r_sys_4resourceroles.txt
+
+		for SYS in `cat $DIR/lssyscfg_r_sys_4resourceroles.txt | grep -v "\." | cut -d "," -f 1`;
+		do
+    		${SSH} ${HMC} "lssyscfg -r lpar -m $SYS -F name,lpar_id,state" > ${DIR}/lssyscfg_r_sys_4resourceroles.txt
+        done
+
+    fi
 
 	if [ ${REPORT} == 'frames_connected' ]
 	then
@@ -112,6 +185,8 @@ do
 	if [ ${REPORT} == 'ALL' ]
 	then
 
+        ${SSH} ${HMC} 'lshmcldap -r config' > ${DIR}/ldap.txt
+        ${SSH} ${HMC} 'lsaccfg  -t resourcerole' > ${DIR}/resourceroles.txt
 		${SSH} ${HMC} 'lssvcevents -t hardware --filter "status=open"'   > ${DIR}/lssvcevents_hardware.txt
 		${SSH} ${HMC} 'lssyscfg -r sys   -F name,serial_num,state'       > ${DIR}/lssyscfg_r_sys_F_name_serial_num_state.txt
 		${SSH} ${HMC} 'lssyscfg -r frame -F name,serial_num'             > ${DIR}/lssysscfg_r_frame_F_name_serial_num_state.txt
@@ -141,8 +216,9 @@ do
     ((HMC_OK=$HMC_OK+1))
 done	
 
-password_remove_file
+#password_remove_file
 
+echo ""
 echo "HMC OK: ${HMC_OK}, HMC no connection: ${HMC_ERRORS}"
 if [ $HMC_OK -gt 0 ]
 then
