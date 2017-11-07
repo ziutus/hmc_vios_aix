e8e1c3f7ad8b3f4aafa57b043c67621b13a16699
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2017-11-07 22:40:47 +0100

    Example of creating lpar.

--- bin/hmc_lpar_create.rb
+++ bin/hmc_lpar_create.rb
@@ -1,74 +1,69 @@
 #! /usr/bin/env ruby
 
-$LOAD_PATH << File.dirname(__FILE__) + './inc'
+$LOAD_PATH << File.dirname(__FILE__) + '/../inc'
 
 require 'optparse'
 require 'yaml'
 require 'net/ssh'
 require 'pp'
 
-require_relative 'inc/nc/MyExec'
-require_relative 'inc/HMC/Sys'
-#require_relative 'inc/HMC/HmcString'
-require_relative 'inc/HMC/Lpar_real'
-require_relative 'inc/HMC/Lpar_profile'
+require 'MyExec'
+require 'HMC/Lpar_profile'
 
-require_relative 'inc/HMC/VirtualEthAdapter'
-require_relative 'inc/HMC/VirtualScsiAdapter'
+lpar_name='test1'
+lpar_id=10
+frame='test'
 
-lparName="test1"
-lparID=10
-frame="test"
-
-if lparName == ""
+if lpar_name == ''
 	puts "if you want to create lpar, please provide name of lpar!\n "
-	puts "exiting..."
+	puts 'exiting...'
 	exit 
 
 end
 
-if lparID == 0
-	puts "You should provide the lparID!"
-	puts "Exiting..."
+if lpar_id == 0
+	puts 'You should provide the lparID!'
+	puts 'Exiting...'
 	exit 10
 end
 
 
-vent1 = VirtualEthAdapter.new()
+vent1 = VirtualEthAdapter.new
 vent1.virtualSlotNumber=2
 vent1.portVlanID=1
 
-vent2 = VirtualEthAdapter.new()
+vent2 = VirtualEthAdapter.new
 vent2.virtualSlotNumber=3
 vent2.portVlanID=2
 
-vscsi1 = VirtualScsiAdapter.new()
+vscsi1 = VirtualScsiAdapter.new
 vscsi1.virtualSlotNumber=4
-vscsi1.clientOrServer="client"
+vscsi1.clientOrServer='client'
 vscsi1.remoteLparID=2
-vscsi1.remoteLparName="vios1"
+vscsi1.remoteLparName='vios1'
 vscsi1.remoteSlotNumber=11
 vscsi1.isRequired=1
 
-vscsi2 = VirtualScsiAdapter.new()
+vscsi2 = VirtualScsiAdapter.new
 vscsi2.virtualSlotNumber=5
-vscsi2.clientOrServer="client"
+vscsi2.clientOrServer='client'
 vscsi2.remoteLparID=3
-vscsi2.remoteLparName="vios2"
+vscsi2.remoteLparName='vios2'
 vscsi2.remoteSlotNumber=12
 vscsi2.isRequired=1	
 
-lpar = Lpar_profile.new(lparID)
-lpar.name = lparName
-lpar.sys = frame
-lpar.adapter_eth_add(vent1)
-lpar.adapter_eth_add(vent2)
+profile = Lpar_profile.new(lpar_id)
+profile.name = lpar_name
+profile.sys = frame
+profile.lpar_id = lpar_id
+profile.lpar_name = lpar_name
+profile.adapter_add(vent1)
+profile.adapter_add(vent2)
 
-lpar.virtual_slots.adapter_scsi_add(vscsi1)
-lpar.adapter_scsi_add(vscsi2)
+profile.adapter_add(vscsi1)
+profile.adapter_add(vscsi2)
 
-#puts Exec.Exec(lpar.get_mksyscfg)
-puts lpar.get_mksyscfg
+puts profile.mksyscfg_cmd
 
 
 exit 0
