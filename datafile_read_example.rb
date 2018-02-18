902e1363f7fefb57bc12137d007c9b1bfcd11994
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-02-18 08:29:17 +0100

    Refactoring of fode.

---
+++ datafile_read_example.rb
@@ -0,0 +1,21 @@
+#! /usr/bin/env ruby
+
+require 'optparse'
+require 'yaml'
+require 'net/ssh'
+require 'pp'
+
+require_relative 'inc/MyExec'
+require_relative 'inc/DataFile'
+
+options = {}
+options = YAML.load_file('hmc_data.yaml')
+
+execMode="on"
+
+Exec = MyExec.new('ssh', execMode)
+Exec.setSsh(options[:hmc], options[:username], options[:password])
+
+File1 = DataFile.new("data/"+options[:hmc]+".txt")
+
+puts File1.Read("lsaccfg -t resourcerole")
