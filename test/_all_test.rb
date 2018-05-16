8ba84b7057f939524109f5ed852dcce8adc2db26
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-05-16 19:53:42 +0200

    removing empty lines.

--- test/_all_test.rb
+++ test/_all_test.rb
@@ -3,8 +3,6 @@ $LOAD_PATH << File.dirname(__FILE__)
 
 require 'test/unit'
 
-
-
 Dir[File.dirname(File.absolute_path(__FILE__)) + '/*_test.rb'].each {|file|
   next if file == '_all_test.rb'
   require file
