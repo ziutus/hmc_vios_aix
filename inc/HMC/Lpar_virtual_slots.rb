f9e58fa12a457449f4440cbd62497a2b6f4af203
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-04 01:02:09 +0200

    Refactoring (Rubocop corrections) part1.

--- inc/HMC/Lpar_virtual_slots.rb
+++ inc/HMC/Lpar_virtual_slots.rb
@@ -12,7 +12,7 @@ class Lpar_virtual_slots
 
   attr_accessor :max_virtual_slots
 
-  #TODO: implement functions for virtual vasi and vsi adapters
+  # TODO: implement functions for virtual vasi and vsi adapters
   attr_reader :virtual_vasi_adapters
   attr_reader :virtual_eth_vsi_profiles
   attr_accessor :virtual_vasi_adapters_raw
@@ -28,12 +28,12 @@ class Lpar_virtual_slots
   attr_reader :virtual_scsi_adapters_raw
   attr_reader :virtual_eth_adapters_raw
 
-  attr_accessor :profile_name #it is helper for diff functions, much easier to say, which profile has different settings
+  attr_accessor :profile_name # it is helper for diff functions, much easier to say, which profile has different settings
 
-  #TODO: check max virtual slots number
-  def initialize(max_virtual_slots = 65000)
+  # TODO: check max virtual slots number
+  def initialize(max_virtual_slots = 65_000)
     @max_virtual_slots = max_virtual_slots
-    @virtual_slots = Hash.new
+    @virtual_slots = {}
 
     @virtual_vasi_adapters    = []
     @virtual_eth_vsi_profiles = []
@@ -44,7 +44,6 @@ class Lpar_virtual_slots
     @virtual_scsi_adapters    = []
     @virtual_serial_adapters  = []
     @virtual_eth_adapters     = []
-
 
     @virtual_fc_adapters_raw      = nil
     @virtual_scsi_adapters_raw    = nil
@@ -58,13 +57,13 @@ class Lpar_virtual_slots
   def virtual_adapter_add(adapter)
 
     case adapter.class.to_s
-      when 'VirtualEthAdapter'    then self.virtual_eth_adapters_add(adapter)
-      when 'VirtualScsiAdapter'   then self.virtual_scsi_adapters_add(adapter)
-      when 'VirtualSerialAdapter' then self.virtual_serial_adapters_add(adapter)
-      when 'VirtualFCAdapter'     then self.virtual_fc_adapters_add(adapter)
-      else
-        pp 'adapter class:' +  adapter.class
-        raise 'unknown type of adapter'
+    when 'VirtualEthAdapter'    then virtual_eth_adapters_add(adapter)
+    when 'VirtualScsiAdapter'   then virtual_scsi_adapters_add(adapter)
+    when 'VirtualSerialAdapter' then virtual_serial_adapters_add(adapter)
+    when 'VirtualFCAdapter'     then virtual_fc_adapters_add(adapter)
+    else
+      pp 'adapter class:' +  adapter.class
+      raise 'unknown type of adapter'
     end
   end
 
@@ -105,8 +104,8 @@ class Lpar_virtual_slots
 
   def virtual_serial_adapters_add(adapter)
     @virtual_serial_adapters.push(adapter)
-#TODO: add this part of code
-#    @virtual_slots[adapter.virtualSlotNumber] = adapter
+    # TODO: add this part of code
+    # @virtual_slots[adapter.virtualSlotNumber] = adapter
 
     if @virtual_serial_adapters_raw.nil?
       @virtual_serial_adapters_raw = adapter.to_s + ','
@@ -120,40 +119,40 @@ class Lpar_virtual_slots
     unless string == 'none'
       HmcString.parse_value(string).each { |adapter|
         case adapter_type
-          when 'virtual_fc_adapters'  then self.virtual_adapter_add(VirtualFCAdapter.new(adapter))
-          when 'virtual_eth_adapters'   then self.virtual_adapter_add(VirtualEthAdapter.new(adapter))
-          when 'virtual_scsi_adapters'  then self.virtual_adapter_add(VirtualScsiAdapter.new(adapter))
-          when 'virtual_serial_adapters' then self.virtual_adapter_add(VirtualSerialAdapter.new(adapter))
-          else
-            raise 'unknown type of virtual adapter'
+        when 'virtual_fc_adapters' then virtual_adapter_add(VirtualFCAdapter.new(adapter))
+        when 'virtual_eth_adapters' then virtual_adapter_add(VirtualEthAdapter.new(adapter))
+        when 'virtual_scsi_adapters' then virtual_adapter_add(VirtualScsiAdapter.new(adapter))
+        when 'virtual_serial_adapters' then virtual_adapter_add(VirtualSerialAdapter.new(adapter))
+        else
+          raise 'unknown type of virtual adapter'
         end
       }
     end
 
     case adapter_type
-      when 'virtual_fc_adapters' then @virtual_fc_adapters_raw = string
-      when 'virtual_eth_adapters' then @virtual_eth_adapters_raw = string
-      when 'virtual_scsi_adapters' then @virtual_scsi_adapters_raw = string
-      when 'virtual_serial_adapters' then @virtual_serial_adapters_raw = string
-      else
-        raise 'unknown type of virtual adapter'
+    when 'virtual_fc_adapters' then @virtual_fc_adapters_raw = string
+    when 'virtual_eth_adapters' then @virtual_eth_adapters_raw = string
+    when 'virtual_scsi_adapters' then @virtual_scsi_adapters_raw = string
+    when 'virtual_serial_adapters' then @virtual_serial_adapters_raw = string
+    else
+      raise 'unknown type of virtual adapter'
     end
   end
 
   def virtual_serial_adapters_raw=(string)
-    self.virtual_adapters_raw(string, 'virtual_serial_adapters')
+    virtual_adapters_raw(string, 'virtual_serial_adapters')
   end
 
   def virtual_eth_adapters_raw=(string)
-    self.virtual_adapters_raw(string, 'virtual_eth_adapters')
+    virtual_adapters_raw(string, 'virtual_eth_adapters')
  end
 
   def virtual_scsi_adapters_raw=(string)
-    self.virtual_adapters_raw(string, 'virtual_scsi_adapters')
+    virtual_adapters_raw(string, 'virtual_scsi_adapters')
   end
 
   def virtual_fc_adapters_raw=(string)
-    self.virtual_adapters_raw(string, 'virtual_fc_adapters')
+    virtual_adapters_raw(string, 'virtual_fc_adapters')
   end
 
   def adapters_to_s(type)
@@ -164,44 +163,44 @@ class Lpar_virtual_slots
     return nil if type == 'virtual_scsi_adapters'   and @virtual_scsi_adapters_raw.nil?
 
     adapters_tmp = case type
-                     when 'virtual_fc_adapters'     then @virtual_fc_adapters
-                     when 'virtual_eth_adapters'    then @virtual_eth_adapters
-                     when 'virtual_scsi_adapters'   then @virtual_scsi_adapters
-                     when 'virtual_serial_adapters' then @virtual_serial_adapters
-                     else
-                       raise 'unknown type ' + type
+                   when 'virtual_fc_adapters'     then @virtual_fc_adapters
+                   when 'virtual_eth_adapters'    then @virtual_eth_adapters
+                   when 'virtual_scsi_adapters'   then @virtual_scsi_adapters
+                   when 'virtual_serial_adapters' then @virtual_serial_adapters
+                   else
+                     raise 'unknown type ' + type
    end
 
     if adapters_tmp.size == 0
       'none'
     else
-      adapters=[]
-      adapters_tmp.each {|adapter|
+      adapters = []
+      adapters_tmp.each do |adapter|
         adapters.push(adapter.to_s)
-      }
+      end
       adapters.join(',')
     end
   end
 
   def virtual_fc_adapters_to_s
-    self.adapters_to_s('virtual_fc_adapters')
+    adapters_to_s('virtual_fc_adapters')
   end
 
   def virtual_scsi_adapters_to_s
-    self.adapters_to_s('virtual_scsi_adapters')
+    adapters_to_s('virtual_scsi_adapters')
   end
 
   def virtual_eth_adapters_to_s
-    self.adapters_to_s('virtual_eth_adapters')
+    adapters_to_s('virtual_eth_adapters')
   end
 
   def virtual_serial_adapters_to_s
-    self.adapters_to_s('virtual_serial_adapters')
+    adapters_to_s('virtual_serial_adapters')
   end
 
   def diff(other_lpar_virtual_slots, type)
 
-  diff = Hash.new
+    diff = {}
     max_virtual_slots = self.max_virtual_slots > other_lpar_virtual_slots.max_virtual_slots ? self.max_virtual_slots : other_lpar_virtual_slots.max_virtual_slots
 
     1.upto(max_virtual_slots) do |i|
@@ -224,7 +223,7 @@ class Lpar_virtual_slots
           end
 
 
-          #let's check if slot are the same type (ethernet, fcs etc)
+          # let's check if slot are the same type (ethernet, fcs etc)
           if self_slot.class.name != other_slot.class.name
             entry = Hash.new
             entry[self_profile_name] =  "The slot type is #{self_slot.class.name}"
@@ -242,17 +241,17 @@ class Lpar_virtual_slots
             diff_entry_slot = self_slot.diff(other_slot, self_profile_name, other_profile_name)
 
             diff_entry_slot.each do |key, entry|
-               diff["VirtualSlot #{i} #{key}"] = entry
+              diff["VirtualSlot #{i} #{key}"] = entry
             end
           else
             raise("unsuported type of adapter #{self_slot.class.name} ")
           end
 
 
-        elsif self.virtual_slots.key?(i) and ! other_lpar_virtual_slots.virtual_slots.key?(i)
+        elsif virtual_slots.key?(i) and ! other_lpar_virtual_slots.virtual_slots.key?(i)
 
-          self_slot =  self.virtual_slots[i]
-          self_profile_name = self.profile_name
+          self_slot = virtual_slots[i]
+          self_profile_name = profile_name
           other_profile_name = other_lpar_virtual_slots.profile_name
 
           # let's check type of slots
@@ -267,14 +266,14 @@ class Lpar_virtual_slots
           end
 
 
-          entry = Hash.new
+          entry = {}
           entry[self_profile_name]  = "A profile use it: #{self_slot.class.name} #{self_slot.to_s}"
           entry[other_profile_name] = "A profile doesn't use slot"
           diff["VirtualSlot #{i}"] = entry
-        elsif  ! self.virtual_slots.key?(i) and other_lpar_virtual_slots.virtual_slots.key?(i)
+        elsif  ! virtual_slots.key?(i) and other_lpar_virtual_slots.virtual_slots.key?(i)
 
           other_slot = other_lpar_virtual_slots.virtual_slots[i]
-          self_profile_name = self.profile_name
+          self_profile_name = profile_name
           other_profile_name = other_lpar_virtual_slots.profile_name
 
           # let's check type of slots
@@ -288,7 +287,7 @@ class Lpar_virtual_slots
             next unless other_slot.class.name == 'VirtualFCAdapter'
           end
 
-          entry = Hash.new
+          entry = {}
           entry[self_profile_name]  = "A profile doesn't use slot"
           entry[other_profile_name] = 'A profile use it: ' + other_slot.class.name + ' ' + other_slot.to_s
 
