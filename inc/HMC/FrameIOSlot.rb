791b240a4ad337b4337841860ca322aa9794021f
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-04 00:28:09 +0200

    Refactoring (Rubocop corrections) part1.

--- inc/HMC/FrameIOSlot.rb
+++ inc/HMC/FrameIOSlot.rb
@@ -30,47 +30,42 @@ class FrameIOSlot
 
 	attr_reader :data_string_raw
 	
-	def initialize string='' 
+	def initialize(string = '')
 
-		@variables_int    = [ 'bus_id', 'iop', 'interposer_present', 'interposer_pcie' ]
+		@variables_int    = %w[ bus_id iop interposer_present interposer_pcie ]
 	
-		@variables_string = [ 'unit_phys_loc', 'phys_loc', 'lpar_id', 'drc_index', 'lpar_name', 'slot_io_pool_id', 'description',
-			'feature_codes', 'pci_vendor_id', 'pci_device_id', 'pci_subs_vendor_id', 'pci_subs_device_id', 
-			'pci_class', 'pci_revision_id', 'bus_grouping', 'parent_slot_drc_index', 'drc_name' ]	
-		
-		if string.length > 0
+		@variables_string = %w[ unit_phys_loc phys_loc lpar_id drc_index lpar_name slot_io_pool_id description
+                            feature_codes pci_vendor_id pci_device_id pci_subs_vendor_id pci_subs_device_id
+                            pci_class pci_revision_id bus_grouping parent_slot_drc_index drc_name ]
+
+		unless string.empty?
 		  @data_string_raw = string
-		  self.parse(string)
-		end			
+		  parse(string)
+		end
 	end
-		
-		
+
+
 	def validate
-#		raise "virtualSlotNumber not defined" unless (@virtualSlotNumber.is_a? Numeric)
+   #	raise "virtualSlotNumber not defined" unless (@virtualSlotNumber.is_a? Numeric)
 	end
 		
 	def to_s
-	
-		self.validate()
-		
-		result
+    validate()
+    result
 	end
 	
-	def parse string
-
-		myHash = HmcString.parse(string)
-	
-		myHash.each { |name, value| 
+	def parse(string)
+		HmcString.parse(string).each do |name, value|
 		
 			if @variables_int.include?(name)
-				instance_variable_set("@#{name}", value.to_i) 
+				instance_variable_set("@#{name}", value.to_i)
 			elsif @variables_string.include?(name)
-				instance_variable_set("@#{name}", value.to_s) 
-			else 
+				instance_variable_set("@#{name}", value.to_s)
+			else
 				print "FrameIOSlot: unknown name: #{name} with value #{value}"
-				raise 
+				raise
 			end
-		}		
+		end
 	end
 
 end
