04a1f3b621479de3edb971ab5bf2be19db375221
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-04 00:41:01 +0200

    Refactoring (Rubocop corrections) part1.

--- inc/HMC/Hmc_upgrade.rb
+++ inc/HMC/Hmc_upgrade.rb
@@ -11,16 +11,16 @@ class HmcUpgrade
   attr_accessor :remove_file
   attr_accessor :interactive
 
-  @hostname       = nil #option -h
-  @user           = nil #option -u
-  @password       = nil #option -p
-  @ssh_key        = nil #?? (version 8?)
-  @mount_location = nil #?? (version 8?)
-  @mount_options  = nil #?? (version 8?)
-  @filename       = nil #option -f (file on the FTP server to obtain or locally)
-  @reboot         = nil #option -r (reboot after installation)
-  @remove_file    = nil #option -c (remove the file from local filesystem after the installation)
-  @interactive    = nil #option -i (prompt for password)
+  @hostname       = nil # option -h
+  @user           = nil # option -u
+  @password       = nil # option -p
+  @ssh_key        = nil # ?? (version 8?)
+  @mount_location = nil # ?? (version 8?)
+  @mount_options  = nil # ?? (version 8?)
+  @filename       = nil # option -f (file on the FTP server to obtain or locally)
+  @reboot         = nil # option -r (reboot after installation)
+  @remove_file    = nil # option -c (remove the file from local filesystem after the installation)
+  @interactive    = nil # option -i (prompt for password)
 
   attr_accessor :_version
 
@@ -30,45 +30,41 @@ class HmcUpgrade
   @_version       = nil
 
   t8_options = %w[ disk dvd ftp sftp nfs usb ]
-  t7_options = %w[ m s l ]  #m - media, s - server, l - local filesystem (only valid )
+  t7_options = %w[ m s l ]  # m - media, s - server, l - local filesystem (only valid )
 
 
   def initialize
-
+    #
   end
-
 
   def validate
 
-    if (@_type.nil?)
-        @_type = 'server' unless @hostname.nil?
-        @_type = 'server' unless @user.nil?
-        @_type = 'server' unless @password.nil?
+    if @_type.nil?
+      @_type = 'server' unless @hostname.nil?
+      @_type = 'server' unless @user.nil?
+      @_type = 'server' unless @password.nil?
     end
 
-
-      if (@_type == 'server')
-        raise 'hostname not setup' if @hostname.nil?
-        raise 'user not setup'     if @user.nil?
-        raise 'password not setup' if @password.nil?
-      end
+    if @_type == 'server'
+      raise 'hostname not setup' if @hostname.nil?
+      raise 'user not setup'     if @user.nil?
+      raise 'password not setup' if @password.nil?
+    end
   end
 
   def cmd
-
-    self.validate
+    validate
 
     command = 'updhmc '
     command += '-r ' if @reboot == true
 
-    if (@_type == 'server')
+    if @_type == 'server'
       command += "-t s -h #{@hostname} -u #{@user} -p #{@password} -f #{@filename}"
-    elsif (@_type == 'media')
+    elsif @_type == 'media'
       raise 'type media - not implemented'
-    elsif (@_type == 'local_filesystem')
+    elsif @_type == 'local_filesystem'
       raise 'type local_filesystem - not implemented'
     end
     command
   end
-
 end
