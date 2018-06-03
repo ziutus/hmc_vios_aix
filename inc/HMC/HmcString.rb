a3debf6260845e9f3ab4ea2c6e212c838a50cac6
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-03 23:53:17 +0200

    Refactoring (Rubocop corrections) part2.

--- inc/HMC/HmcString.rb
+++ inc/HMC/HmcString.rb
@@ -2,103 +2,86 @@ module HmcString
 
   def parse(string)
 
-    myResult = {}
+    my_result = {}
     key = ''
     value = ''
-    insideIndex = 1
-    insiteQuotationMark = 0
+    inside_index = 1
+    insite_quotation_mark = 0
 
-	i=0	
-	while i < string.length
+    i = 0
+    while i < string.length
 
-	  if string[i] == '"' and string[i+1] == '"' 
-      i=i+2
-      value << '""'
-      next
+      if string[i] == '"' && string[i + 1] == '"'
+        i += 2
+        value << '""'
+        next
+      end
+
+      if string[i] == '"' && string[i + 1] != '"'
+        insite_quotation_mark ==0 ? insite_quotation_mark =1 : insite_quotation_mark = 0
+        i += 1
+        next
+      end
+
+      if string[i] == ',' && insite_quotation_mark.zero?
+        my_result[key] = value
+        key = ''
+        value = ''
+        inside_index = 1
+        i += 1
+        next
+      end
+
+      if string[i] == '=' && inside_index == 1
+        inside_index = 0
+        i += 1
+        next
+      end
+
+      inside_index == 1 ? key << string[i] : value << string[i]
+      i += 1
     end
-	
-    if string[i] == '"' and string[i+1] != '"'
-      insiteQuotationMark ==0 ? insiteQuotationMark =1 : insiteQuotationMark = 0
-      i=i+1
-      next
-    end
-
-    if string[i] == ',' and insiteQuotationMark == 0
-      myResult[key]= value
-      key = ''
-      value = ''
-      insideIndex = 1
-      i=i+1
-      next
-    end
-
-
-    if string[i] == "=" and insideIndex == 1
-      insideIndex = 0
-      i+=1
-      next
-    end
-
-
-    if insideIndex == 1
-       key << string[i]
-    else
-      value << string[i]
-    end
-  
-	  i+=1
-	end
-    myResult[key]= value #last key and value (loop is ended as we haven't last ",")
-
-    myResult
+    my_result[key]= value # last key and value (loop is ended as we haven't last ",")
+    my_result
   end
-  
-  def parse_value string 
-  
-	i=0	
-  insiteQuotationMark = 0
-	key=''
-	value=''
-	array = []
 
-	while i < string.length
+  def parse_value(string)
+    i = 0
+    insite_quotation_mark = 0
+    value = ''
+    array = []
 
-		if string[i] == '"' and string[i+1] == '"'
-			if insiteQuotationMark == 0
-				insiteQuotationMark = 1
-			else 
-				insiteQuotationMark = 0
-			end
-			
-			i=i+2
-			next
-		end
-		
-		if string[i] == ',' and insiteQuotationMark == 0
-			array.push(value)
-			value = ''
-			i=i+1
-			next
-		end
+    while i < string.length
+      if string[i] == '"' && string[i + 1] == '"'
+        insite_quotation_mark.zero? ? insite_quotation_mark = 1 : insite_quotation_mark = 0
+        i += 2
+        next
+      end
 
-		value << string[i]
-		i=i+1
-	end
+      if string[i] == ',' && insite_quotation_mark.zero?
+        array.push(value)
+        value = ''
+        i += 1
+        next
+      end
 
-	array.push(value)
-	array
+      value << string[i]
+      i += 1
+    end
+
+    array.push(value)
+    array
   end
 
   def make_string(parameter, string)
 
     unless string.nil?
       string = parameter + '=' + string
-      if string.include?('"') or string.include?(',')
+      if string.include?('"') || string.include?(',')
         string = '"' + string + '"'
       end
     end
 
     string
   end
-
 end
