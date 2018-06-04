3a6a77f28b041a4a2f6c870649c2dde8c1d08333
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-04 09:32:33 +0200

    Refactoring (Rubocop corrections).

--- inc/HMC/ResourceRole.rb
+++ inc/HMC/ResourceRole.rb
@@ -6,80 +6,69 @@ require 'pp'
 
 class ResourceRole
 
-	attr_reader :name
-	attr_reader :resources 
+  attr_reader :name
+  attr_reader :resources
 
-	#name=L2support,"resources=cec:root/ibmhscS1_0|9131-52A*6535CCG|IBMHSC_ComputerSystem,lpar:root/ibmhscS1_0|ALL_PARTITIONS*9131-52A*6535CCG|IBMHSC_Partition"
-	def initialize(string='')
+  # name=L2support,"resources=cec:root/ibmhscS1_0|9131-52A*6535CCG|IBMHSC_ComputerSystem,lpar:root/ibmhscS1_0|ALL_PARTITIONS*9131-52A*6535CCG|IBMHSC_Partition"
+  def initialize(string='')
+    @resources = []
 
-		@resources = Array.new
-		
-		if string.length > 0
-			self.decode string 
-		end	
-	end 
-	
+    decode(string) unless string.empty?
+  end
 
-	def decode(string)
-
+  def decode(string)
     raise 'new line character in string' if string.include?("\n")
 
-		if match = /^name=([\w\-\_]+),"resources=(.*?)"$/.match(string)
+    if match = /^name=([\w\-\_]+),"resources=(.*?)"$/.match(string)
 
-			@name=match[1]
-			match[2].split(',').each { |resource|
-				@resources.push(Resource.new(resource))
-			}
+      @name = match[1]
+      match[2].split(',').each do |resource|
+        @resources.push(Resource.new(resource))
+      end
     elsif match = /^name=([\w\-\_]+),resources=$/.match(string)
-
-      @name=match[1]
+      @name = match[1]
     elsif match = /^name=([\w\-\_]+),resources=(.*?)$/.match(string)
 
-        @name=match[1]
-        match[2].split(',').each { |resource|
-          @resources.push(Resource.new(resource))
-        }
-		else
-			puts string 
-			puts "regexp couldn't decode string"
-			raise 
-		end
-		
-
-	end
+      @name = match[1]
+      match[2].split(',').each do |resource|
+        @resources.push(Resource.new(resource))
+      end
+    else
+      puts string
+      puts "regexp couldn't decode string"
+      raise
+    end
+  end
 
   def has_all_partitions?(type_model_serial)
     @resources.each { |resource|
       if resource.type == 'lpar'
-        if resource.frame == type_model_serial and resource.lpar == 'ALL_PARTITIONS'
-          return true
-        end
+        return true if resource.frame == type_model_serial && resource.lpar == 'ALL_PARTITIONS'
       end
     }
     false
   end
 
-	def has_lpar?(type_model_serial, lpar_id)
-		
-		@resources.each { |resource|
-				if resource.type == 'lpar'
-					if resource.frame == type_model_serial and resource.lpar == 'ALL_PARTITIONS'
-						return true
-					end
+  def has_lpar?(type_model_serial, lpar_id)
+    @resources.each do |resource|
+      if resource.type == 'lpar'
+        if resource.frame == type_model_serial && resource.lpar == 'ALL_PARTITIONS'
+          return true
+        end
 
-					if resource.frame == type_model_serial and resource.lpar == lpar_id.to_s
-						return true
-					end
-				end
-		}		
-		
-		false
-	end
+        return true if resource.frame == type_model_serial && resource.lpar == lpar_id.to_s
+      end
+    end
+
+    false
+  end
 
   def has_all_lpars?(type_model_serial)
-    @resources.each { |resource|
-      return true if resource.type == 'lpar' if resource.frame == type_model_serial and resource.lpar == 'ALL_PARTITIONS'
-    }
+    @resources.each do |resource|
+      if resource.type == 'lpar'
+        return true if resource.frame == type_model_serial && resource.lpar == 'ALL_PARTITIONS'
+      end
+    end
     false
   end
 end
