2281777d7085380bfd292c3abc6c16a39ddbf1f4
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-04 12:07:50 +0200

    Refactoring (Rubocop corrections).

--- inc/Targetcli/Backstore_block_lv.rb
+++ inc/Targetcli/Backstore_block_lv.rb
@@ -1,51 +1,45 @@
 class Backstore_block_lv < Backstore_block
 
-	attr_accessor :vg
-	attr_accessor :lv
+  attr_accessor :vg
+  attr_accessor :lv
 
-	def initialize 
-		super 		
+  def initialize
+    super
+  end
 
-	end
-	
-	def lsDecode string
-	
-		regexp_ls_backstore =  /^\s*\|\s+\|\s+\o\-\s+([\w\_]+)\s+[\.]+\s+\[\/dev\/([\w\_]+)\/([\w\_]+)\s+\((\d+\.\d+[MG]iB)\)\s+(write-thru)\s(activated)\]$/
+  def lsDecode string
 
-		if %r{^No\ssuch\spath\s}.match(string)
-			'no device'
-		elsif match_ls_backstore = regexp_ls_backstore.match(string)
-		
-			@name = match_ls_backstore[1]
-			@vg = match_ls_backstore[2]
-			@lv = match_ls_backstore[3]
-			@size = match_ls_backstore[4]
-			@write = match_ls_backstore[5]
-			@status = match_ls_backstore[6]
-		
-			@path = '/dev/' + @vg + '/' +@lv
-		
-			return 'device found'
-		else
-			
-			puts "RegExp couldn't decode string >#{string}<"
-			raise "Couldn't decode string"
-			
-		end
-		
-	end	
-	
-	def create_cmd
+    regexp_ls_backstore =  /^\s*\|\s+\|\s+\o\-\s+([\w\_]+)\s+[\.]+\s+\[\/dev\/([\w\_]+)\/([\w\_]+)\s+\((\d+\.\d+[MG]iB)\)\s+(write-thru)\s(activated)\]$/
 
-		@command + ' /backstores/block create name=' + @name + ' dev=/dev/' + @vg + '/' + @lv
-	end
+    if %r{^No\ssuch\spath\s}.match(string)
+      'no device'
+    elsif match_ls_backstore = regexp_ls_backstore.match(string)
 
-	def delete_cmd
-	
-		@command + ' /backstores/block delete ' + @name
-	end 
-	
-	def ls_all_cmd
-		@command + ' /backstores/block ls'
-	end 
-end 
+      @name = match_ls_backstore[1]
+      @vg = match_ls_backstore[2]
+      @lv = match_ls_backstore[3]
+      @size = match_ls_backstore[4]
+      @write = match_ls_backstore[5]
+      @status = match_ls_backstore[6]
+
+      @path = '/dev/' + @vg + '/' +@lv
+
+      return 'device found'
+    else
+      puts "RegExp couldn't decode string >#{string}<"
+      raise "Couldn't decode string"
+    end
+  end
+
+  def create_cmd
+    @command + ' /backstores/block create name=' + @name + ' dev=/dev/' + @vg + '/' + @lv
+  end
+
+  def delete_cmd
+    @command + ' /backstores/block delete ' + @name
+  end
+
+  def ls_all_cmd
+    @command + ' /backstores/block ls'
+  end
+end
