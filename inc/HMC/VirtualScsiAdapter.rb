9a6562ac751fe3b5480ed833112245a1565aa72d
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-04 11:31:14 +0200

    Refactoring (Rubocop corrections).

--- inc/HMC/VirtualScsiAdapter.rb
+++ inc/HMC/VirtualScsiAdapter.rb
@@ -1,6 +1,6 @@
 class VirtualScsiAdapter
 
-	#TODO: analyze if in case that remote lpar or slot is 'any' can we do simpler code (not so many if..elsif...)
+	# TODO: analyze if in case that remote lpar or slot is 'any' can we do simpler code (not so many if..elsif...)
 
 	attr_accessor :virtualSlotNumber
 	attr_accessor :clientOrServer
@@ -9,14 +9,24 @@ class VirtualScsiAdapter
 	attr_accessor :remoteSlotNumber
 	attr_accessor :isRequired
 
+  attr_accessor :lpar_name
+  attr_accessor :lpar_id
+  attr_accessor :state
+  attr_reader :_type
+
+
 	def initialize string='' 
-		@virtualSlotNumber
-		@clientOrServer
-		@remoteLparID
-		@remoteLparName
-		@remoteSlotNumber
-		@isRequired 
-		
+		@virtualSlotNumber = nil
+		@clientOrServer = nil
+		@remoteLparID = nil
+		@remoteLparName = nil
+		@remoteSlotNumber = nil
+		@isRequired = nil
+    @lpar_name = nil
+    @lpar_id = nil
+
+    @_type = 'profile'
+
 		if string.length > 0
 		  @data_string_raw = string
 		  self.parse(string)
@@ -26,7 +36,11 @@ class VirtualScsiAdapter
 	#virtual-slot-number/client-or-server/[remote-lpar-ID]/[remote-lpar-name]/[remote-slot-number]/is-required
 	def to_s
 		self.validate
-		"#{@virtualSlotNumber}/#{@clientOrServer}/#{@remoteLparID}/#{@remoteLparName}/#{@remoteSlotNumber}/#{@isRequired}"
+    if @_type == 'real'
+      "lpar_name=#{@lpar_name},lpar_id=#{@lpar_id},slot_num=#{@virtualSlotNumber},state=#{@state},is_required=#{@isRequired},adapter_type=#{@clientOrServer},remote_lpar_id=#{@remoteLparID},remote_lpar_name=#{@remoteLparName},remote_slot_num=#{@remoteSlotNumber}"
+    else
+      "#{@virtualSlotNumber}/#{@clientOrServer}/#{@remoteLparID}/#{@remoteLparName}/#{@remoteSlotNumber}/#{@isRequired}"
+    end
 	end
 	
 	def validate
@@ -53,14 +67,28 @@ class VirtualScsiAdapter
 		raise "isRequired has wrong value #{@isRequired} " unless (@isRequired == 0 or @isRequired == 1)
 		
 	end 
-	
-	def decode string
+
+  def can_parse?(string)
+
+    regExp      = %r{^\s*(\d+)/(server|client)/(\d+)/([\w\_\-\.]+|)/(\d+)/(0|1)\s*$}
+    regExp_any  = %r{^\s*(\d+)/(server|client)/(any)/([\w\_\-\.]+|)/(any)/(0|1)\s*$}
+    regExp_long = %r{^\s*slot_num=(\d+),state=(0|1),is_required=(0|1),adapter_type=(client|server),remote_lpar_id=(\d+|any),remote_lpar_name=([\w\_\-]+|),remote_slot_num=(\d+|any)\s*$}
+    regExp_real = %r{^\s*lpar_name=([\w\_\-]+),lpar_id=(\d+),slot_num=(\d+),state=(0|1),is_required=(0|1),adapter_type=(client|server),remote_lpar_id=(\d+|any),remote_lpar_name=([\w\_\-]+|),remote_slot_num=(\d+|any)\s*$}
+
+    return true if  match = regExp.match(string)
+    return true if  match = regExp_any.match(string)
+    return true if  match = regExp_long.match(string)
+    return true if  match = regExp_real.match(string)
+
+    false
+  end
+
+	def decode(string)
 
 		regExp      = %r{^\s*(\d+)/(server|client)/(\d+)/([\w\_\-\.]+|)/(\d+)/(0|1)\s*$}
 		regExp_any  = %r{^\s*(\d+)/(server|client)/(any)/([\w\_\-\.]+|)/(any)/(0|1)\s*$}
-		regExp_long = %{^\s*slot_num=(\d+),state=(0|1),is_required=(0|1),adapter_type=(client|server),remote_lpar_id=(\d+|any),remote_lpar_name=([\w\_\-]+|),remote_slot_num=(\d+|any)\s*$}
-		
-		
+		regExp_long = %r{^\s*slot_num=(\d+),state=(0|1),is_required=(0|1),adapter_type=(client|server),remote_lpar_id=(\d+|any),remote_lpar_name=([\w\_\-]+|),remote_slot_num=(\d+|any)\s*$}
+    regExp_real = %r{^\s*lpar_name=([\w\_\-]+),lpar_id=(\d+),slot_num=(\d+),state=(0|1),is_required=(0|1),adapter_type=(client|server),remote_lpar_id=(\d+|any),remote_lpar_name=([\w\_\-]+|),remote_slot_num=(\d+|any)\s*$}
 		
 		if  match = regExp.match(string)
 			@virtualSlotNumber	= match[1].to_i	
@@ -78,17 +106,29 @@ class VirtualScsiAdapter
 			@isRequired			= match[6].to_i
 		elsif match = regExp_long.match(string)
 			@virtualSlotNumber	= match[1].to_i	
-			@state				= match[2]
+			@state				= match[2].to_i
 			@isRequired			= match[3].to_i
 			@clientOrServer		= match[4]
 			@remoteLparID		= match[5].to_i
 			@remoteLparName		= match[6]
 			@remoteSlotNumber	= match[7].to_i
-		else	
+    elsif match = regExp_real.match(string)
+
+      @lpar_name = match[1]
+      @lpar_id = match[2].to_i
+      @virtualSlotNumber	= match[3].to_i
+      @state				= match[4].to_i
+      @isRequired			= match[5].to_i
+      @clientOrServer		= match[6]
+      @remoteLparID		= match[7].to_i
+      @remoteLparName		= match[8]
+      @remoteSlotNumber	= match[9].to_i
+      @_type = 'real'
+		else
 			abort "Class VirtualScsiAdapter:RegExp couldn't decode string #{string}"
-		end		
+		end
 	end
-	
+
 	alias :parse :decode
 
 	def ==(another_adapter)
