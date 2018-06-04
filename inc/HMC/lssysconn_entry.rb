5f04035f70d7ec2156e13c18c1d497ffba420195
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-04 09:07:59 +0200

    Refactoring (Rubocop corrections).

--- inc/HMC/lssysconn_entry.rb
+++ inc/HMC/lssysconn_entry.rb
@@ -27,7 +27,6 @@ class Lssysconn_entry
   attr_accessor :owner
 
   def initialize(string = nil)
-
     @resource_type = nil
     @type_model_serial_num = nil
     @sp = nil
@@ -37,7 +36,7 @@ class Lssysconn_entry
     @connection_error_code = nil
 
     @hmc_real = nil
-    @hmc_expected = Array.new()
+    @hmc_expected = []
 
     @owner = nil # in case of type 'sys', owner is fame name
 
@@ -47,43 +46,42 @@ class Lssysconn_entry
     @name = nil
 
     @_states = [
-        'pending authentication - password updates required',
-        'No Connection',
-        'Failed Authentication',
-        'Connecting',
-        'Connected',
-        'Version Mismatch'
+      'pending authentication - password updates required',
+      'No Connection',
+      'Failed Authentication',
+      'Connecting',
+      'Connected',
+      'Version Mismatch'
     ]
 
     @_errors = [
-        'Incorrect password \w{4}-\w{4}-\w{8}',
-        'Connecting\s+\w{4}-\w{4}-\w{8}',
-        'Connection not allowed\s+\w{4}-\w{4}-\w{8}',
-        'Already connected\s+\w{4}-\w{4}-\w{8}',
-        'Firmware Password locked\s+\w{4}-\w{4}-\w{8}',
-        'Version\smismatch\s+\w{4}-\w{4}-\w{8}'
+      'Incorrect password \w{4}-\w{4}-\w{8}',
+      'Connecting\s+\w{4}-\w{4}-\w{8}',
+      'Connection not allowed\s+\w{4}-\w{4}-\w{8}',
+      'Already connected\s+\w{4}-\w{4}-\w{8}',
+      'Firmware Password locked\s+\w{4}-\w{4}-\w{8}',
+      'Version\smismatch\s+\w{4}-\w{4}-\w{8}'
     ]
 
-    self.parse(string) unless string.nil?
+    parse(string) unless string.nil?
   end
 
   def parse(string)
-
     states = @_states.join('|')
     errors = @_errors.join('|')
     sp = 'primary|secondary|unavailable'
     ip = '\d+\.\d+\.\d+\.\d+'
     tmsn = '\w{4}\-\w{3}\*\w{7,8}|unavailable'
-    sp_phys_loc='[\w\.\-]+|unavailable'
+    sp_phys_loc = '[\w\.\-]+|unavailable'
 
 
-    #resource_type=sys,type_model_serial_num=9117-570*100729E,sp=unavailable,ipaddr=10.0.0.247,alt_ipaddr=unavailable,state=No Connection,connection_error_code=Connecting 0000-0000-00000000
+    # resource_type=sys,type_model_serial_num=9117-570*100729E,sp=unavailable,ipaddr=10.0.0.247,alt_ipaddr=unavailable,state=No Connection,connection_error_code=Connecting 0000-0000-00000000
     if match = /resource_type=(sys),type_model_serial_num=(#{tmsn}),sp=(#{sp}),ipaddr=(#{ip}),alt_ipaddr=(unavailable),state=(#{states}),connection_error_code=(Connecting \w{4}-\w{4}-\w{8})/i.match(string)
       @resource_type = match[1]
       @type_model_serial_num = match[2]
       @sp = match[3]
       @ipaddr = match[4]
-      @alt_ipaddr  = match[5]
+      @alt_ipaddr = match[5]
       @state = match[6]
       @connection_error_code = match[7]
 
@@ -95,7 +93,7 @@ class Lssysconn_entry
       @sp = match[3]
       @sp_phys_loc = match[4]
       @ipaddr = match[5]
-      @alt_ipaddr  = match[6]
+      @alt_ipaddr = match[6]
       @state = match[7]
       @connection_error_code = match[8]
 
@@ -107,11 +105,10 @@ class Lssysconn_entry
       @sp = match[3]
       @sp_phys_loc = match[4]
       @ipaddr = match[5]
-      @alt_ipaddr  = match[6]
+      @alt_ipaddr = match[6]
       @state = match[7]
 
       @type, @model, @serial_num = type_model_serial_num_to_array(@type_model_serial_num)
-
 
     elsif match = /resource_type=(frame),type_model_serial_num=(#{tmsn}),side=(a|b),ipaddr=(#{ip}),state=(#{states})/i.match(string)
 
@@ -129,7 +126,7 @@ class Lssysconn_entry
       @type_model_serial_num = match[2]
       @side = match[3]
       @ipaddr = match[4]
-      @alt_ipaddr  = match[5]
+      @alt_ipaddr = match[5]
       @state = match[6]
 
       @type, @model, @serial_num = type_model_serial_num_to_array(@type_model_serial_num)
@@ -140,7 +137,7 @@ class Lssysconn_entry
       @type_model_serial_num = match[2]
       @side = match[3]
       @ipaddr = match[4]
-      @alt_ipaddr  = match[5]
+      @alt_ipaddr = match[5]
       @state = match[6]
       @connection_error_code = match[7]
 
@@ -150,12 +147,11 @@ class Lssysconn_entry
       pp string
       raise 'wrong string to parse'
     end
-
   end
 
   def type_model_serial_num_to_array(string)
-    if ( match =/(\w{4})\-(\w{3})\*(\w{7,8})/.match(string) )
-        return [match[1], match[2], match[3]]
+    if (match = /(\w{4})\-(\w{3})\*(\w{7,8})/.match(string))
+      [match[1], match[2], match[3]]
     end
   end
 
