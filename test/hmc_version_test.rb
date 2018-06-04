42290f2eb7aba877f35938372a696b483ce26513
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-04 10:13:21 +0200

    Refactoring (Rubocop corrections).

--- test/hmc_version_test.rb
+++ test/hmc_version_test.rb
@@ -15,13 +15,13 @@ HMC Build level 20141006.3
 ","base_version=V8R8.2.0
 "'
 
-    myHmc = Version.new(string)
+    my_hmc = Version.new(string)
 
-    assert_equal('8',          myHmc.version)
-    assert_equal('8.2.0',      myHmc.release)
-    assert_equal('0',          myHmc.servicePack)
-    assert_equal('20141006.3', myHmc.hmcBuildLevel)
-    assert_equal('V8R8.2.0',   myHmc.base_version)
+    assert_equal('8',          my_hmc.version)
+    assert_equal('8.2.0',      my_hmc.release)
+    assert_equal('0',          my_hmc.servicePack)
+    assert_equal('20141006.3', my_hmc.hmcBuildLevel)
+    assert_equal('V8R8.2.0',   my_hmc.base_version)
 
   end
 
@@ -35,14 +35,14 @@ MH01454: Required fix for HMC V8R8.2.0 (11-05-2014)
 ","base_version=V8R8.2.0
 "'
 
-    myHmc = Version.new(string)
+    my_hmc = Version.new(string)
 
-    assert_equal('8',          myHmc.version)
-    assert_equal('8.2.0',      myHmc.release)
-    assert_equal('0',          myHmc.servicePack)
-    assert_equal('20141104.1', myHmc.hmcBuildLevel)
-    assert_equal(true,         myHmc.hasFix?('MH01454'))
-    assert_equal('V8R8.2.0',   myHmc.base_version)
+    assert_equal('8',          my_hmc.version)
+    assert_equal('8.2.0',      my_hmc.release)
+    assert_equal('0',          my_hmc.servicePack)
+    assert_equal('20141104.1', my_hmc.hmcBuildLevel)
+    assert_equal(true,         my_hmc.hasFix?('MH01454'))
+    assert_equal('V8R8.2.0',   my_hmc.base_version)
 
   end
 end
