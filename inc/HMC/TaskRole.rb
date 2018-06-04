87ff29d269e9189c95927e42be7e3e1033bcf8f6
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-04 10:02:53 +0200

    Refactoring (Rubocop corrections).

--- inc/HMC/TaskRole.rb
+++ inc/HMC/TaskRole.rb
@@ -15,9 +15,7 @@ class TaskRole
 
   def initialize(string)
 
-    if string.length > 0
-      self.parse(string)
-    end
+    parse(string) unless string.empty?
   end
 
   #name=hmcservicerep,parent=Predefined,"resources=""frame:CheckPSN+DeactivateFrameIO+FrameInitialize+LaunchFrameAsm+ListFrameProperty+ManageFrameServEvents+ManagePSN+RefreshFrame,cec:ActivateSystemProfile+BackupProfileData+CECPowerOff+CECPowerOn+CaptureSystemTemplate+CollectCECVPDInfo+DeployPartitionTemplate+DeviceMaintenance+LSProfileSpace+LaunchAsm+ListCECProperty+ListCoDInformation+ListNPortLogin+ListPCIeTopology+ListRioTopology+ListSSP+ListSystemProfileProperty+ListUtilizationData+ListVETInfo+ManageCECServEvents+ManageDumps+ManageSPP+ManageSysProfile+ManageUtilizationData+RebuildCEC+UpdateLIC+ValidateSystemProfile+ViewDumps+ViewPowerManagement+ViewSPP,lpar:ActivateLPAR+CapturePartitionTemplate+ChangeNPortLogin+CloseVTerm+Connect5250VTerm+DlparOperation+ListLPARProperty+ListProfileProperty+ManageLPARServEvents+ManageProfile+Open5250VTerm+OpenVTerm+RebootLPAR+ShutdownLPAR+VirtualIOServerCommand,HMCConsole:5250Task+BackupHMCData+ChangeHMCConfiguration+ChangeLocale+ChangeUserPasswords+CollectVPDInfo+CustServiceSettings+FormatMedia+GetUpgradeFiles+GroupingApp+ListConnections+ListHMCConfiguration+ListSNMPServiceSettings+ListServiceableEvents+ListStorageMedia+ManageConsoleDumps+ManageSNMPServiceSettings+ManageServEvents+RepairServEvent+RestoreUpgradeData+SaveUpgradeData+ShutdownHMC+TemplateLibrary+TipOfTheDay+UpdateHMC+UserSettings+ViewConsoleEvents+ViewHMCFileSystems"""
@@ -25,10 +23,8 @@ class TaskRole
 
     hash = HmcString.parse(string)
 
-    #pp hash
-
-    @name = hash["name"]
-    @parent = hash["parent"]
+    @name = hash['name']
+    @parent = hash['parent']
     @resources_raw = hash['resources']
 
     parseResorces(@resources_raw)
@@ -36,20 +32,13 @@ class TaskRole
   end
 
   def parseResorces(string)
-
-
-      regexp = /frame:(.*),cec:(.*),HMCConsole:(.*)/
-      match = regexp.match(string)
-      if (match)
-        #puts( "frame to:" + match[1])
-        resources_frame = match[1].split("+")
-        #puts( "cec   to:" + match[2])
-        #puts( "HMCco to:" + match[3])
-
-        #pp resources_frame
-      else
-        raise "can't find regexp"
-      end
+    regexp = /frame:(.*),cec:(.*),HMCConsole:(.*)/
+    match = regexp.match(string)
+    if (match)
+      resources_frame = match[1].split("+")
+    else
+      raise "can't find regexp"
+    end
 
   end
 
