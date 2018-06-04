cb1d0073089dacc04b307309200fa4229224f8cf
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-04 08:50:54 +0200

    Refactoring (Rubocop corrections).

--- inc/HMC/lspartition_dlpar.rb
+++ inc/HMC/lspartition_dlpar.rb
@@ -2,35 +2,24 @@ require 'pp'
 
 class LspartitionDlpar
 
-
   attr_reader :data
   attr_reader :data_string_raw
 
   def initialize(string)
-
-    @data = Hash.new
+    @data = {}
     @data_string_raw=''
 
-
-    if string.length > 0
-      @data_string_raw = string
-      self.parse(string)
-    end
+    parse(string) unless string.empty?
   end
 
   def parse(string)
+    @data_string_raw = string
 
-    string.gsub!("\nActive", "Active")
+    string.gsub!("\nActive", 'Active').split("\n").each do |line|
 
-    ##pp string
+      if match = /\<\#(\d+)\>\s+Partition:\<\d+\*\w{4}\-\w{3}\*\w{6,7}\,\s+([\w\.]+)\,\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\>Active:\<(0|1)\>\,\sOS:\<(AIX|[\s\,]+)\>,\sDCaps:\<0x2f\>\,\sCmdCaps:\<0x0b,\s0x0b\>,\sPinnedMem:\<(\d+)\>/.match(line)
 
-    string.split("\n").each { |line|
-
-        #pp line
-
-    if match = /\<\#(\d+)\>\s+Partition:\<\d+\*\w{4}\-\w{3}\*\w{6,7}\,\s+([\w\.]+)\,\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\>Active:\<(0|1)\>\,\sOS:\<(AIX|[\s\,]+)\>,\sDCaps:\<0x2f\>\,\sCmdCaps:\<0x0b,\s0x0b\>,\sPinnedMem:\<(\d+)\>/.match(line)
-
-        row = Hash.new
+        row = {}
         row['entry'] = match[1].to_i
         row['lpar_id'] = match[2].to_i
         row['system'] = match[3]
@@ -40,15 +29,12 @@ class LspartitionDlpar
         row['DCaps'] = match[7]
         row['CmdCaps'] = match[8]
         row['PinnedMem'] = match[9]
-        #
+
         @data[row['entry']] = row
       else
         raise Exception, "Wrong string >#{line}"
       end
-
-    }
-
-
+    end
   end
 
 end
