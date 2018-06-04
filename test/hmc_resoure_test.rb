7ac9b789783a0e0ba83404141ed794c3250a9688
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-04 09:20:48 +0200

    Refactoring (Rubocop corrections).

--- test/hmc_resoure_test.rb
+++ test/hmc_resoure_test.rb
@@ -7,49 +7,47 @@ require 'HMC/Resource'
 
 class TestHMCResource < Test::Unit::TestCase
 
-	def test_name
-		string = 'lpar:root/ibmhscS1_0|1*9131-52A*6535CCG|IBMHSC_Partition'
+  def test_name
+    string = 'lpar:root/ibmhscS1_0|1*9131-52A*6535CCG|IBMHSC_Partition'
 
-		resource = Resource.new(string)
-		
-		assert_equal('IBMHSC_Partition', resource.type_long)
-		assert_equal('lpar',             resource.type )
-		assert_equal('1',                resource.lpar )
-		assert_equal('9131-52A*6535CCG', resource.frame)
-		
-	end
+    resource = Resource.new(string)
 
-	def test_name2
-		string = 'lpar:root/ibmhscS1_0|ALL_PARTITIONS*9131-52A*6535CCG|IBMHSC_Partition'
+    assert_equal('IBMHSC_Partition', resource.type_long)
+    assert_equal('lpar',             resource.type )
+    assert_equal('1',                resource.lpar )
+    assert_equal('9131-52A*6535CCG', resource.frame)
 
-		resource = Resource.new(string)
+  end
 
-		assert_equal('IBMHSC_Partition', resource.type_long, )
-		assert_equal('lpar',             resource.type, )
-		assert_equal('ALL_PARTITIONS',   resource.lpar)
-		assert_equal('9131-52A*6535CCG', resource.frame)
-	end
+  def test_name2
+    string = 'lpar:root/ibmhscS1_0|ALL_PARTITIONS*9131-52A*6535CCG|IBMHSC_Partition'
 
-	#test data taken from: https://www-304.ibm.com/webapp/set2/.../hmc_best_practices.pdf
-	def test_cec
-		string = 'cec:root/ibmhscS1_0|9406-520*10007CA|IBMHSC_ComputerSystem'
-		resource = Resource.new(string)
+    resource = Resource.new(string)
 
-		assert_equal('IBMHSC_ComputerSystem', resource.type_long )
-		assert_equal('cec',             resource.type )
-		assert_equal('9406-520*10007CA', resource.frame)
-	end
+    assert_equal('IBMHSC_Partition', resource.type_long, )
+    assert_equal('lpar',             resource.type, )
+    assert_equal('ALL_PARTITIONS',   resource.lpar)
+    assert_equal('9131-52A*6535CCG', resource.frame)
+  end
 
-	#source: own power5 frame
-	def test_frame
-		string = 'frame:root/ibmhscS1_0|9131-52A*6535CCG|IBMHSC_Frame'
-		resource = Resource.new(string)
+  # test data taken from: https://www-304.ibm.com/webapp/set2/.../hmc_best_practices.pdf
+  def test_cec
+    string = 'cec:root/ibmhscS1_0|9406-520*10007CA|IBMHSC_ComputerSystem'
+    resource = Resource.new(string)
 
-		assert_equal('IBMHSC_Frame', resource.type_long )
-		assert_equal('frame',             resource.type )
-		assert_equal('9131-52A*6535CCG', resource.frame)
-	end
+    assert_equal('IBMHSC_ComputerSystem', resource.type_long)
+    assert_equal('cec', resource.type)
+    assert_equal('9406-520*10007CA', resource.frame)
+  end
 
+  # source: own power5 frame
+  def test_frame
+    string = 'frame:root/ibmhscS1_0|9131-52A*6535CCG|IBMHSC_Frame'
+    resource = Resource.new(string)
 
-end			
-		
+    assert_equal('IBMHSC_Frame', resource.type_long)
+    assert_equal('frame', resource.type)
+    assert_equal('9131-52A*6535CCG', resource.frame)
+  end
+
+end
