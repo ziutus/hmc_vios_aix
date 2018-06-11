bfd1c68f6a9a8169053f60a5e95de3681a05ef98
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-11 06:49:18 +0200

    New tests

--- test/hmc_VirtualEthAdapter_test.rb
+++ test/hmc_VirtualEthAdapter_test.rb
@@ -64,6 +64,43 @@ class TestVirtualEthAdapter < Test::Unit::TestCase
     assert_equal(true, adapter.can_parse?('6/0/6//2/1'))
     assert_equal(true, adapter.can_parse?('10/0/1//0/0/ETHERNET0//all/none'))
     assert_equal(true, adapter.can_parse?('2/1/1/3,4,5/0/1'))
+    assert_equal(true, adapter.can_parse?('lpar_name=dump,lpar_id=1,slot_num=2,state=0,is_required=1,is_trunk=0,ieee_virtual_eth=0,port_vlan_id=1,addl_vlan_ids=,mac_addr=26E6B0001002'))
+  end
+
+  # data source: own frame
+  def test_parse_real
+    string = 'lpar_name=dump,lpar_id=1,slot_num=2,state=0,is_required=1,is_trunk=0,ieee_virtual_eth=0,port_vlan_id=1,addl_vlan_ids=,mac_addr=26E6B0001002'
+    adapter = VirtualEthAdapter.new(string)
+    assert_equal('dump', adapter.lpar_name)
+    assert_equal(1, adapter.lpar_id)
+    assert_equal(2, adapter.virtualSlotNumber)
+    assert_equal(0, adapter.state)
+    assert_equal(1, adapter.isRequired)
+    assert_equal(0, adapter.trunkPriority)
+    assert_equal(0, adapter.isIEEE)
+    assert_equal(1, adapter.portVlanID)
+    assert_equal('',adapter.additionalVlanIDs)
+    assert_equal('26E6B0001002', adapter.macAddress)
+
+    assert_equal(string, adapter.to_s)
+  end
+
+  def test_parse_real2
+    string = 'lpar_name=vios2,lpar_id=3,slot_num=8,state=1,is_required=1,is_trunk=1,trunk_priority=2,ieee_virtual_eth=0,port_vlan_id=8,addl_vlan_ids=,mac_addr=26E6B0003008'
+
+    adapter = VirtualEthAdapter.new(string)
+    assert_equal('vios2', adapter.lpar_name)
+    assert_equal(3, adapter.lpar_id)
+    assert_equal(8, adapter.virtualSlotNumber)
+    assert_equal(1, adapter.state)
+    assert_equal(1, adapter.isRequired)
+    assert_equal(2, adapter.trunkPriority)
+    assert_equal(0, adapter.isIEEE)
+    assert_equal(8, adapter.portVlanID)
+    assert_equal('',adapter.additionalVlanIDs)
+    assert_equal('26E6B0003008', adapter.macAddress)
+
+    assert_equal(string, adapter.to_s)
   end
 
   def test_validation
