922636067565c89a5283d8d0466a2a4107cf8217
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-20 00:01:25 +0200

    Tabs to spaces

--- inc/VIOS/Vtd2.rb
+++ inc/VIOS/Vtd2.rb
@@ -1,92 +1,92 @@
 class Vtd2
 
-	#see: https://www.ibm.com/support/knowledgecenter/en/POWER8/p8hcg/p8hcg_lsmap.htm
-	
-	attr_accessor :name, :status, :lun, :backing_device, :physloc, :mirrored 
-#	attr_accessor :data_string_raw
-	
-	def initialize string='' 
-		@name 
-		@status 
-		@lun 
-		@backing_device 
-		@physloc
-		@mirrored 		
-		
-		if string.length > 0
-#		  @data_string_raw = string
-		  self.parse(string)
-		end			
-	end
-	
-	def decode string
+  #see: https://www.ibm.com/support/knowledgecenter/en/POWER8/p8hcg/p8hcg_lsmap.htm
 
-	
-		regExp =  %r{^\s*VTD\s+([\w\_\-]+)\s*
+  attr_accessor :name, :status, :lun, :backing_device, :physloc, :mirrored
+#	attr_accessor :data_string_raw
+
+  def initialize string=''
+    @name
+    @status
+    @lun
+    @backing_device
+    @physloc
+    @mirrored
+
+    if string.length > 0
+#		  @data_string_raw = string
+      self.parse(string)
+    end
+  end
+
+  def decode string
+
+
+    regExp =  %r{^\s*VTD\s+([\w\_\-]+)\s*
 Status\s+(Available|Defined)\s+
 LUN\s+(0x\w+)\s+
 Backing\sdevice\s+([\w\-\_\.]+)\s+
 Physloc\s+([\w\.\-]+)\s+
 Mirrored\s+(false|true)\s*$}mx
 
-		regExp2 =  %r{^\s*VTD\s+([\w\_\-]+)\s*
+    regExp2 =  %r{^\s*VTD\s+([\w\_\-]+)\s*
 Status\s+(Available|Defined)\s+
 LUN\s+(0x\w+)\s+
 Backing\sdevice\s+([\w\-\_\.]+)\s+
 Physloc\s*$}mx
 
-		regExp3 =  %r{^\s*VTD\s+([\w\_\-]+)\s*
+    regExp3 =  %r{^\s*VTD\s+([\w\_\-]+)\s*
 LUN\s+(0x\w+)\s+
 Backing\sdevice\s+([\w\-\_\.]+)\s+
 Physloc\s*$}mx
 
-		regExp4 =  %r{^\s*VTD\s+([\w\_\-]+)\s*
+    regExp4 =  %r{^\s*VTD\s+([\w\_\-]+)\s*
 Status\s+(Available|Defined)\s+
 LUN\s+(0x\w+)\s+
 Backing\sdevice\s+([\w\-\_\.]+)\s+
 Physloc\s+([\w\.\-]+)\s*$}mx
 
-		if match = regExp.match(string)
-		
-			@name 			= match[1]
-			@status 		= match[2]
-			@lun 			= match[3]
-			@backing_device = match[4]
-			@physloc		= match[5]
-			@mirrored 		= match[6]	
+    if match = regExp.match(string)
 
-		elsif match = regExp2.match(string)
-		
-			@name 			= match[1]
-			@status 		= match[2]
-			@lun 			= match[3]
-			@backing_device = match[4]
-			@physloc		= match[5].to_s
-			
-		elsif match = regExp3.match(string)
-		
-			@name 			= match[1]
-			@lun 			= match[2]
-			@backing_device = match[3]
-			@physloc		= match[4].to_s
+      @name 			= match[1]
+      @status 		= match[2]
+      @lun 			= match[3]
+      @backing_device = match[4]
+      @physloc		= match[5]
+      @mirrored 		= match[6]
 
-		elsif match = regExp4.match(string)
-		
-			@name 			= match[1]
-			@status 		= match[2]
-			@lun 			= match[3]
-			@backing_device = match[4]
-			@physloc		= match[5].to_s
-			
-		else 
-			raise "VIOS->Vtd: parse: RegExp couldn't decode string >>#{string}<<"
-		
-		end
+    elsif match = regExp2.match(string)
 
-		
-	end
+      @name 			= match[1]
+      @status 		= match[2]
+      @lun 			= match[3]
+      @backing_device = match[4]
+      @physloc		= match[5].to_s
 
-	alias :parse :decode
+    elsif match = regExp3.match(string)
 
-	
+      @name 			= match[1]
+      @lun 			= match[2]
+      @backing_device = match[3]
+      @physloc		= match[4].to_s
+
+    elsif match = regExp4.match(string)
+
+      @name 			= match[1]
+      @status 		= match[2]
+      @lun 			= match[3]
+      @backing_device = match[4]
+      @physloc		= match[5].to_s
+
+    else
+      raise "VIOS->Vtd: parse: RegExp couldn't decode string >>#{string}<<"
+
+    end
+
+
+  end
+
+  alias :parse :decode
+
+
 end
