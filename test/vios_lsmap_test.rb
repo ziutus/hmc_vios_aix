058a59a41d7a8f1b163ca0ad40dcce6654208067
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-19 21:07:40 +0200

    Refactoring (Rubocop corrections).

--- test/vios_lsmap_test.rb
+++ test/vios_lsmap_test.rb
@@ -14,16 +14,15 @@ class TestVioslsmap_long < Test::Unit::TestCase
 # https://www.ibm.com/developerworks/community/forums/html/topic?id=77777777-0000-0000-0000-000014422737
 
 
-	def test_vtd1
-		lsmap = Lsmap.new(File.read('data/vios_lsmap_data.txt'))
+  def test_vtd1
+    lsmap = Lsmap.new(File.read('data/vios_lsmap_data.txt'))
 
-		assert_equal(0,	lsmap.mapping['vhost0'].vtds.count)
-		assert_equal(3,	lsmap.mapping['vhost2'].vtds.count)
-		assert_equal(4,	lsmap.mapping['vhost4'].vtds.count)
-		assert_equal(0,	lsmap.mapping['vhost3'].vtds.count)
+    assert_equal(0,	lsmap.mapping['vhost0'].vtds.count)
+    assert_equal(3,	lsmap.mapping['vhost2'].vtds.count)
+    assert_equal(4,	lsmap.mapping['vhost4'].vtds.count)
+    assert_equal(0,	lsmap.mapping['vhost3'].vtds.count)
 
-
-    vhosts =  lsmap.mapping_for_lpar(4)
+    vhosts = lsmap.mapping_for_lpar(4)
 
     assert_equal(2, vhosts.count, 'lpar 4 has 2 vhosts')
 
