717e1225247045748d3a8735bcd84b149c876831
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-06-20 21:11:27 +0200

    Errpt support.

--- inc/AIX/Errpt.rb
+++ inc/AIX/Errpt.rb
@@ -1,45 +1,45 @@
-require "pp"
+require 'pp'
 
-require "AIX/ErrptEntry"
+require 'AIX/ErrptEntry'
 
 class Errpt
 
-	attr_reader :data_string_raw
+  attr_reader :data_string_raw
   attr_reader :errors
 
 
-	def initialize(string)
+  def initialize(string)
+    @data_string_raw=''
+    @errors = []
+    @errors_summary = {}
 
-		@data_string_raw=''
-    @errors = Array.new
+    parse(string) unless string.empty?
+  end
 
-		if string.length > 0
-		  self.parse(string)
-		end
-
-	end 
-
-	def parse(string)
-
+  def parse(string)
     @data_string_raw = string
 
-		if match = %r{LABEL:\s+([\w\_]+)\s$}.match(string)
-			# it should be parse in different way
+    if match = %r{LABEL:\s+([\w\_]+)\s$}.match(string)
+      # it should be parse in different way
       raise 'this script can not now parse errpt -a output'
     else
       string.split("\n").each { |line|
+        next if line =~ /^\s*IDENTIFIER\s+TIMESTAMP\s+T\s+C\s+RESOURCE_NAME\s+DESCRIPTION\s*$/
         @errors.push(ErrptEntry.new(line))
       }
-		end
+    end
   end
 
   def summary
-    @errors.each { |errpt|
+    @errors_summary = {}
+    @errors.each do |errpt|
       if errpt.description == 'BACK-UP PATH STATUS CHANGE'
-
+        # powerpath issue
       end
 
-    }
+
+
+    end
   end
 
 end
--- inc/AIX/ErrptEntry.rb
+++ inc/AIX/ErrptEntry.rb
@@ -1,32 +1,28 @@
-require "pp"
+require 'pp'
 
 class ErrptEntry
 
-	attr_reader :data_string_raw
-	attr_reader :label
-	attr_reader :identifier
-	attr_reader :datetime
-	attr_reader :datetime_long
-	attr_reader :type
-	attr_reader :class
-	attr_reader :description
-	attr_reader :resource_name
-	
-#	attr_accessor :break_size
-	
-	def initialize(string)
+  attr_reader :data_string_raw
+  attr_reader :label
+  attr_reader :identifier
+  attr_reader :datetime
+  attr_reader :datetime_long
+  attr_reader :type
+  attr_reader :class
+  attr_reader :description
+  attr_reader :resource_name
+  #	attr_accessor :break_size
 
-		@data_string_raw=''
+  def initialize(string)
+    @data_string_raw = ''
 
-		if string.length > 0
-		  @data_string_raw = string
-		  self.parse(string)
-		end
-	end 
+    parse(string) unless string.empty?
+  end
 
-	def parse(string)
+  def parse(string)
+    @data_string_raw = string
 
-		regexp_long = %r{
+    regexp_long = %r{
 LABEL:\s+([\w\_]+)\s+
 IDENTIFIER:\s+(\w+)\s+
 Date\/Time:\s+(\w{3}\s\w{3}\s+\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2}\s\w{3}\s\d{4})\s+
@@ -47,30 +43,30 @@ Failure\sCauses(.*)\s+
 Detail\sData(.*)\s+
 SENSE\sDATA(.*)}mx
 
-		if match      = %r{(\w{8})\s+(\d{10})\s+(T|P|I)\s+(S|H|O|U)\s+(\w+)\s+(.*)}.match(string)
+    if match = %r{(\w{8})\s+(\d{10})\s+(T|P|I)\s+(S|H|O|U)\s+(\w+)\s+(.*)}.match(string)
 
-			@identifier 	= match[1]
-			@datetime		= match[2]	
-			@type			= match[3]
-			@class			= match[4]		
-			@resource_name	= match[5]		
-			@description	= match[6]		
-		
-		elsif match_long = regexp_long.match(string)
-		
-			@label 			= match_long[1]	
-			@identifier 	= match_long[2]
-			@datetime_long	= match_long[3]	
+      @identifier 	  = match[1]
+      @datetime		    = match[2]
+      @type			      = match[3]
+      @class			    = match[4]
+      @resource_name	= match[5]
+      @description	  = match[6]
 
-	
-	    else
-		  #puts "can't analyze string, regexp is not working"
-		  #puts string
-		  #puts regexp
-		  #puts match
-		  puts "regexp couldn't decode string #{string}"
-		  raise
-		end
+    elsif match_long = regexp_long.match(string)
 
-	end
+      @label 			    = match_long[1]
+      @identifier 	  = match_long[2]
+      @datetime_long	= match_long[3]
+
+
+      else
+      # puts "can't analyze string, regexp is not working"
+      # puts string
+      # puts regexp
+      # puts match
+      puts "regexp couldn't decode string #{string}"
+      raise
+    end
+
+  end
 end
