c401f519b02523ab2c58ced5fae26a1a0eb459a2
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-07-02 19:59:06 +0200

    new tests added.

--- test/aix_fcstat_test.rb
+++ test/aix_fcstat_test.rb
@@ -30,7 +30,7 @@ VPD information not found'
     # example output from fcstat command is taken from IBM website:
     # https://www.ibm.com/support/knowledgecenter/en/ssw_aix_61/com.ibm.aix.cmds2/fcstat.htm
     # some data were changed to next numbers to be sure that regexp is working correctly
-    string = File.read('data/fcstat.txt')
+    string = File.read('test/data/fcstat.txt')
 
     fcstat = Fcstat.new(string)
     data = fcstat.get_stats()
@@ -96,4 +96,144 @@ VPD information not found'
     assert_equal(209883136, fcstat.data['FC SCSI Traffic Statistics']['Output Bytes'])
 
   end
+
+  # test data source: http://blog.krystalia.net/post/2007/08/28/aix-nouvel-utilitaire-fcstat.html
+  def test_fcstat_long_2
+
+    string = File.read('test/data/fcstat_2.txt')
+  fcstat = Fcstat.new(string)
+  data = fcstat.get_stats()
+
+  assert_equal('fcs0', fcstat.device, 'fcstat device name')
+  assert_equal('fcs0', data['device'], 'fcstat device name')
+
+  assert_equal('fcs0',     fcstat.data['device'], 'fcstat device name')
+
+  assert_equal('FC Adapter (df1000fd)', fcstat.data['Device Type'])
+  assert_equal('1B9999999',            fcstat.data['Serial Number'])
+  assert_equal('02C82138', fcstat.data['Option ROM Version'])
+  assert_equal('B1F2.10X8', fcstat.data['Firmware version'])
+
+  assert_equal('0x20000000C964777E', fcstat.data['Node WWN'])
+  assert_equal('0x10000000C964777E', fcstat.data['Port WWN'])
+
+  assert_equal('0x0000012000000000000000000000000000000000000000000000000000000000', fcstat.data["FC4 Types"]['Supported'], 'fcstat device name')
+  assert_equal('0x0000010000000000000000000000000000000000000000000000000000000000', fcstat.data["FC4 Types"]['Active'],    'fcstat device name')
+
+  assert_equal('3', fcstat.data['Class of Service'])
+
+  assert_equal('0x610213', fcstat.data['Port FC ID'])
+  assert_equal('4 GBIT', fcstat.data['Port Speed (supported)'])
+  assert_equal('4 GBIT', fcstat.data['Port Speed (running)'])
+  assert_equal('Fabric', fcstat.data['Port Type'])
+  assert_equal(1_049_191,  fcstat.data['Seconds Since Last Reset'])
+
+  assert_equal(2877134108, fcstat.data['Transmit Statistics']['Frames'])
+  assert_equal(1099511627520, fcstat.data['Transmit Statistics']['Words'])
+  assert_equal(1261755420, fcstat.data['Receive Statistics']['Frames'])
+  assert_equal(586414879232, fcstat.data['Receive Statistics']['Words'])
+
+  assert_equal(0,  fcstat.data['LIP Count'])
+  assert_equal(0,  fcstat.data['NOS Count'])
+  assert_equal(0,  fcstat.data['Error Frames'])
+  assert_equal(0,  fcstat.data['Dumped Frames'])
+  assert_equal(0,  fcstat.data['Link Failure Count'])
+  assert_equal(6, fcstat.data['Loss of Sync Count'])
+  assert_equal(0, fcstat.data['Loss of Signal'])
+  assert_equal(0, fcstat.data['Primitive Seq Protocol Err Count'])
+  assert_equal(0, fcstat.data['Invalid Tx Word Count'])
+  assert_equal(0, fcstat.data['Invalid CRC Count'])
+
+  assert_equal(0, fcstat.data['IP over FC Adapter Driver Information']['No DMA Resource Count'])
+  assert_equal(0, fcstat.data['IP over FC Adapter Driver Information']['No Adapter Elements Count'])
+
+  assert_equal(0, fcstat.data['FC SCSI Adapter Driver Information']['No DMA Resource Count'])
+  assert_equal(316556, fcstat.data['FC SCSI Adapter Driver Information']['No Adapter Elements Count'])
+  assert_equal(2045709, fcstat.data['FC SCSI Adapter Driver Information']['No Command Resource Count'])
+
+  assert_equal(0, fcstat.data['IP over FC Traffic Statistics']['Input Requests'])
+  assert_equal(0, fcstat.data['IP over FC Traffic Statistics']['Output Requests'])
+  assert_equal(0, fcstat.data['IP over FC Traffic Statistics']['Control Requests'])
+  assert_equal(0, fcstat.data['IP over FC Traffic Statistics']['Input Bytes'])
+  assert_equal(0, fcstat.data['IP over FC Traffic Statistics']['Output Bytes'])
+
+  assert_equal(35188289, fcstat.data['FC SCSI Traffic Statistics']['Input Requests'])
+  assert_equal(47812736, fcstat.data['FC SCSI Traffic Statistics']['Output Requests'])
+  assert_equal(890940, fcstat.data['FC SCSI Traffic Statistics']['Control Requests'])
+  assert_equal(2312815985241, fcstat.data['FC SCSI Traffic Statistics']['Input Bytes'])
+  assert_equal(5720318319104, fcstat.data['FC SCSI Traffic Statistics']['Output Bytes'])
+
+
+  end
+
+  # test data source: http://lokams.blogspot.com/2008/08/how-to-find-world-wide-name-wwn-of.html
+  def test_fcstat_long_3
+
+    string = File.read('test/data/fcstat_2.txt')
+    fcstat = Fcstat.new(string)
+    data = fcstat.get_stats()
+  end
+
+  def test_fc_scsi_traffic_statistics
+    string = 'FC SCSI Traffic Statistics
+ Input Requests: 35188289
+ Output Requests: 47812736
+ Control Requests: 890940
+ Input Bytes: 2312815985241
+ Output Bytes: 5720318319104'
+    fcstat = Fcstat.new
+    data = fcstat.parse_fc_scsi_traffic_statistics(string)
+
+    assert_equal(35188289, data['Input Requests'])
+    assert_equal(47812736, data['Output Requests'])
+    assert_equal(890940, data['Control Requests'])
+    assert_equal(2312815985241, data['Input Bytes'])
+    assert_equal(5720318319104, data['Output Bytes'])
+
+  end
+
+  def test_parse_ip_over_fc_traffic_statistics
+    string = 'IP over FC Traffic Statistics
+ Input Requests: 1
+ Output Requests: 2
+ Control Requests: 3
+ Input Bytes: 4
+ Output Bytes: 5'
+
+    fcstat = Fcstat.new
+    data = fcstat.parse_ip_over_fc_traffic_statistics(string)
+
+    assert_equal(1, data['Input Requests'])
+    assert_equal(2, data['Output Requests'])
+    assert_equal(3, data['Control Requests'])
+    assert_equal(4, data['Input Bytes'])
+    assert_equal(5, data['Output Bytes'])
+  end
+
+  def test_fc_scsi_adapter_driver_information
+    string = 'FC SCSI Adapter Driver Information
+ No DMA Resource Count: 0
+ No Adapter Elements Count: 316556
+ No Command Resource Count: 2045709
+'
+    fcstat = Fcstat.new
+    data = fcstat.parse_fc_scsi_adapter_driver_information(string)
+    assert_equal(0,         data['No DMA Resource Count'])
+    assert_equal(316_556,   data['No Adapter Elements Count'])
+    assert_equal(2_045_709, data['No Command Resource Count'])
+
+  end
+
+  def test_parse_ip_over_fc_adapter_2
+    string = 'IP over FC Adapter Driver Information
+  No DMA Resource Count: 15
+  No Adapter Elements Count: 16'
+    fcstat = Fcstat.new
+    data = fcstat.parse_ip_over_fc_adapter_driver_information(string)
+
+    assert_equal(15, data['No DMA Resource Count'])
+    assert_equal(16, data['No Adapter Elements Count'])
+
+  end
+
 end
