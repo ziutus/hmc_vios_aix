5a057589884d27b139ac8acabc7082ee69295562
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-07-07 00:37:19 +0200

    Refactoring + adding plans for new functions.

--- inc/Framework/DataFile.rb
+++ inc/Framework/DataFile.rb
@@ -1,11 +1,25 @@
 class DataFile
 
-  def initialize(fileName)
-    @filename  = fileName
+  #TODO: create table with aliases of commands
+  # so if you call command as padmin or using swrole, it shouldn't be difference as result is the same
+
+  attr_accessor :separator
+
+  def initialize(filename, separator = nil)
+    @filename = filename
+    @separator = separator.empty? ? '| |' : separator
+
+    unless File.readable?(@filename)
+      puts "Can't read file #{@filename}" if $DEBUG
+      return false
+    end
   end
 
-  def find(command, separator = '| |')
+  def find(command, separator = nil)
     data_string = ''
+    separator = @separator if separator.empty?
+
+    return false unless File.readable?(@filename)
 
     File.open(@filename).each do |line|
       tmp = line.split(separator)
@@ -14,12 +28,24 @@ class DataFile
     data_string
   end
 
-  def Write(command, value)
+  def validate
+    # each file should have server name, date, version and "---end---" (to validate that all data are taken)
+    #
+    # TODO: function will return array with errors, if empty, no issues
+  end
+
+  def how_old?
+    # TODO: function will return seconds from date written in file (it will hep to check if check is not too  old)
+  end
+
+  def write(command, value)
     File.open(@filename, 'a') do |file|
       value.split("\n").each do |line|
         file.write(command + '| |' + line + "\n")
       end
     end
   end
+
+  alias Write write
 
 end
