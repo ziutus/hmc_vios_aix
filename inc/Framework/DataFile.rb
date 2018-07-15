a93a8493aac0b5a436b77675326606bd93f4170e
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2018-07-15 22:13:46 +0200

    The class can working on Windows and Linux, verbose can be setup now, file can be created and filled with data(useful during creating test data), find command can use alias table (when you are searching some data but file has similar data collected using different command).

--- inc/Framework/DataFile.rb
+++ inc/Framework/DataFile.rb
@@ -4,55 +4,28 @@ class DataFile
   # so if you call command as padmin or using swrole, it shouldn't be difference as result is the same
 
   attr_accessor :separator
+  attr_accessor :verbose
 
   attr_reader :commands
 
+
   def initialize(filename, separator = nil)
+
+    filename.gsub!('/', "\\") if filename =~ /\w:\\/
+
     @filename = filename
     @separator = separator.nil? ? '| |' : separator
 
-    unless File.readable?(@filename)
-      puts "Can't read file #{@filename}" if $DEBUG
-      return false
-    end
-
-    @aliases = { 'lsmap -npiv -all' => '/usr/ios/cli/ioscli lsmap -npiv -all' }
     @commands = []
 
+    @verbose = 0
   end
 
-  def list_of_commands
-    return false unless File.readable?(@filename)
-    separator = @separator
-    commands = []
+  def create(version, lparname, date)
 
-    File.open(@filename).each do |line|
-      tmp = line.split(separator)
-      commands.push(tmp[0]) unless commands.include?(tmp[0])
-    end
-  end
-
-  def find(command, separator = nil)
-    data_string = ''
-    separator = @separator if separator.nil? or separator.empty?
-
-    return nil unless File.readable?(@filename)
-
-    File.open(@filename).each do |line|
-      tmp = line.split(separator)
-      data_string += tmp[1] if tmp[0] =~ /^\s*#{command}\s*(?:2>&1\s*|)$/
-    end
-    data_string
-  end
-
-  def validate
-    # each file should have server name, date, version and "---end---" (to validate that all data are taken)
-    #
-    # TODO: function will return array with errors, if empty, no issues
-  end
-
-  def how_old?
-    # TODO: function will return seconds from date written in file (it will hep to check if check is not too  old)
+    write('ScriptVersion', version)
+    write('uname -n', lparname)
+    write('date', date)
   end
 
   def write(command, value)
@@ -61,6 +34,101 @@ class DataFile
         file.write(command + '| |' + line + "\n")
       end
     end
+  end
+
+
+  def close(command, value)
+    write('echo "---end---"', '---end---')
+  end
+
+
+  def list_of_commands
+    return false unless File.readable?(@filename)
+    separator = @separator
+    @commands = []
+
+    File.open(@filename).each do |line|
+      tmp = line.split(separator)
+      @commands.push(tmp[0]) unless @commands.include?(tmp[0])
+    end
+  end
+
+  def find(command, separator = nil)
+
+    unless File.readable?(@filename)
+      puts "file #{@filename} is not readable!"
+      return nil
+     end
+
+    list_of_commands if @commands.empty?
+
+    similar_command = find_simiar_command(command)
+    command = similar_command unless similar_command.nil?
+
+    data_string = ''
+    separator = @separator if separator.nil? or separator.empty?
+
+    File.open(@filename).each do |line|
+      tmp = line.split(separator)
+      data_string += tmp[1] if tmp[0] =~ /^\s*#{command}\s*(?:2>&1\s*|)$/
+    end
+    data_string
+  end
+
+  def find_simiar_command(command)
+
+    # TODO: move list of commands to separete file, this part should be easier to update
+    if command.include?('lsmap') and command.include?('-all')  and command.include?('npiv')
+      aliases = [
+          'lsmap -npiv -all',
+          'lsmap -npiv -all -fmt :',
+          'lsmap -all -npiv',
+          'lsmap -all -npiv -fmt :',
+          '/usr/ios/cli/ioscli lsmap -npiv -all',
+          '/usr/ios/cli/ioscli lsmap -npiv -all -fmt :',
+          '/usr/ios/cli/ioscli lsmap -all -npiv',
+          '/usr/ios/cli/ioscli lsmap -all -npiv -fmt :',
+      ]
+
+      aliases.each do |command|
+        return command if @commands.include?(command)
+      end
+    elsif command.include?('lsmap') and command.include?('-all')
+
+      aliases = [
+          'lsmap -all',
+          'lsmap -all -fmt :',
+          '/usr/ios/cli/ioscli lsmap -all',
+          '/usr/ios/cli/ioscli lsmap -all -fmt :',
+      ]
+
+      aliases.each do |command|
+        return command if @commands.include?(command)
+      end
+
+    end
+    nil
+  end
+
+  def validate
+    # each file should have server name, date, version and "---end---" (to validate that all data are taken)
+    #
+    # TODO: function will return array with errors, if empty, no issues
+    list_of_commands
+    return false unless @commands.include?('date')
+    return false unless @commands.include?('uname -n')
+    return false unless @commands.include?('ScriptVersion')
+    retunr false unless @commands.include?('echo "---end---"')
+    true
+  end
+
+  def how_old?
+    # TODO: function will return seconds from date written in file (it will hep to check if check is not too  old)
+  end
+
+  def make_empty
+    File.open(@filename, 'w') { |file| file.truncate(0) }
+    @commands = []
   end
 
   alias Write write
