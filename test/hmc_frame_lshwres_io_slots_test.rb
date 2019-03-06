beb57a049b2b7f71ff6d7bd31c5cd0a350b870f9
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2019-03-06 11:00:04 +0100

    Corrected path to test file.

--- test/hmc_frame_lshwres_io_slots_test.rb
+++ test/hmc_frame_lshwres_io_slots_test.rb
@@ -9,7 +9,7 @@ class TestFrameIOSlots < Test::Unit::TestCase
 
 	def test_validation
 
-		slot = FrameIOSlots.new(File.read('data/hmc_frame_lshwres_io_slots.txt'))
+		slot = FrameIOSlots.new(File.read('test/data/hmc_frame_lshwres_io_slots.txt'))
 		slot.get_lpar('vios2')
 	end
 end
