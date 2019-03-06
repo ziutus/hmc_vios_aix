3eff82efb848f3ade3a5db5071b0f8abc43fc3fd
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2019-03-06 18:16:08 +0100

    Draft version of script.

---
+++ bin/aix_check_packages_version.rb
@@ -0,0 +1,41 @@
+#! /usr/bin/env ruby
+
+require 'pp'
+require 'optparse'
+
+$LOAD_PATH << File.dirname(__FILE__) + '/../inc'
+
+
+OptionParser.new do |opts|
+  opts.on('-d', '--directory NAME', "base directory with all data, default: '#{directory}'") { |v| directory = v }
+  opts.on('-D', '--date DATE', 'date and time of collected data')         { |v| date = v }
+  opts.on('-L', '--lpar LPAR_TO_FIND', 'name of lpar to find')         { |v| lpar_to_find = v }
+  opts.on('--verbose LEVEL', Integer, 'Level of verbose of script') { |v| verbose = v }
+
+  opts.on('-h', '--help', 'Print this help') do
+    puts opts
+    exit
+  end
+end.parse!
+
+if date.nil?
+  puts "The 'date' is not provided, please use --help to see options, exiting..."
+  exit 4
+end
+
+puts "Lpar to find: >#{lpar_to_find}<"
+
+hmc_data = FrameworkData.new(directory, date, config_file)
+hmc_data.verbose = verbose
+lpars = hmc_data.search_lpar(lpar_to_find)
+
+lpars.each do |lpar|
+  pp lpar.hmc
+  pp lpar.sys
+  pp lpar.name
+  pp lpar.vioses
+#  pp lpar.npiv
+#  pp lpar.vscsi.to_s
+end
+
+exit errors
