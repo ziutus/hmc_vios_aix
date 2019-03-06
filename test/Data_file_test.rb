001de6edd054b33fb3e4de858dc42895dffddc0b
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2019-03-06 18:24:38 +0100

    Tests for data files.

--- test/Data_file_test.rb
+++ test/Data_file_test.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH << File.dirname(__FILE__)+'/../inc'
 $LOAD_PATH << File.dirname(__FILE__)+'./inc'
 
-require 'DataFile'
+require 'Framework/DataFile'
 
 require 'test/unit'
 require 'pp'
@@ -10,10 +10,17 @@ class DataFileTest < Test::Unit::TestCase
 
 
   def test_read_data_file_1
-    datafile = DataFile.new('data/data_file.txt')
+    datafile = DataFile.new('test/data/data_file.txt')
 
     assert_equal("command_4_result_line_1\n", datafile.find('command 4 2>&1'))
     assert_equal("command_4_result_line_1\n", datafile.find('command 4'))
+
+  end
+
+  def test_similar_commands
+    datafile = DataFile.new('test/data/data_file_lsmap.txt')
+    assert_equal('lsmap -npiv -all -fmt :', datafile.find_simiar_command('lsmap -npiv -all'))
+    assert_equal('lsmap -all', datafile.find_simiar_command('lsmap -all -fmt :'))
 
   end
 end
