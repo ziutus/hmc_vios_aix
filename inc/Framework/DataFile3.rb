1fa0d489abe9995da2ec37938877280414b7b60d
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2019-03-25 22:23:27 +0100

    New function read_simple (in case command is only once time in file), attributes added filename and debug (for better debug options), some debug options, command rexexp is corrected.

--- inc/Framework/DataFile3.rb
+++ inc/Framework/DataFile3.rb
@@ -1,10 +1,17 @@
 class DataFile3
 
-  def initialize(filename, comment = nil)
-    @filename = filename
+  attr_reader :filename
+  attr_writer :debug
 
-    unless File.exist?(filename)
+  def initialize(filename, comment = nil, debug = false)
+    @filename = filename
+    @debug = debug
+
+    if File.exist?(filename)
+      puts "File exist #{filename}" if @debug
+    else
       File.open(@filename, 'a') do |file|
+        puts "File exist #{filename}, putting version information" if @debug
         file.write("### file format:3.0 ###\n")
       end
 
@@ -18,7 +25,7 @@ class DataFile3
 
   def write(command, value)
     File.open(@filename, 'a') do |file|
-      file.write("### command:#{command} ### time:#{Time.now.strftime("%Y-m-d_%H:%M:%S")} ### timestamp: #{Time.now.to_i} ###\n")
+      file.write("### command:#{command} ### time:#{Time.now.strftime("%Y-%m-%d_%H:%M:%S")} ### timestamp: #{Time.now.to_i} ###\n")
       file.write(value)
     end
   end
@@ -29,7 +36,7 @@ class DataFile3
 
     file = File.new(@filename, 'r')
     while (line = file.gets)
-      if match = /### command: (.*) ### time:(.*) ### timestamp: (\d+) ###/.match(line)
+      if match = /### command:(.*)### time:(.*) ### timestamp: (\d+) ###/.match(line)
         result[match[1]] = [] if result[match[1]].nil?
         result[match[1]].push(match[3])
       end
@@ -47,7 +54,7 @@ class DataFile3
     file = File.new(@filename, 'r')
     while (line = file.gets)
 
-      if match = /### command: (.*) ### time:(.*) ### timestamp: (\d+) ###/.match(line)
+      if match = /### command:(.*) ### time:(.*) ### timestamp: (\d+) ###/.match(line)
         match[1] == command ? insite_command = 1 : insite_command = 0
         timestamp = match[3]
         next
@@ -66,5 +73,19 @@ class DataFile3
     result
   end
   alias Read read
+
+  def read_simple(command)
+
+    data_string = ''
+    data = read(command)
+
+    if data.count == 1
+      data_string = data[data.keys[0]]
+      return data_string
+    else
+      return false
+    end
+
+  end
 
 end
