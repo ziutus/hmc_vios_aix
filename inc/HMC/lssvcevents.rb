b98a407dda6b0dd0be5c61165b1c287ca45b03d0
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2019-04-26 09:55:29 +0200

    Refactoring of code.

--- inc/HMC/lssvcevents.rb
+++ inc/HMC/lssvcevents.rb
@@ -1,8 +1,7 @@
-
 require 'HMC/lssvcenevents_entry'
 
+# Fantastic class to analyze events from HMCs
 class Lssvcevents
-
   attr_reader :events
   attr_reader :errors
 
@@ -14,12 +13,10 @@ class Lssvcevents
   end
 
   def parse(string, hmc_name)
+    return true if string =~ /^No results were found./
 
-    return true if   /^No results were found./.match(string)
-
-    if /An unknown error occurred while trying to perform this command. Retry the command. If the error persists, contact your software support representative/.match(string)
-      hash_data = { :hmc => hmc_name, :error => 'An unknown error occurred while trying to perform this command. Retry the command. If the error persists, contact your software support representative' }
-      @errors.push(hash_data)
+    if string =~ /An unknown error occurred while trying to perform this command. Retry the command. If the error persists, contact your software support representative/
+      @errors.push(hmc: hmc_name, error: 'An unknown error occurred while trying to perform this command. Retry the command. If the error persists, contact your software support representative')
       return true
     end
 
@@ -33,13 +30,13 @@ class Lssvcevents
 
       exist_in_events = false
       event_id = 0
-      @events.each_with_index  { |event, id|
+      @events.each_with_index do |event, id|
         if event.compare(entry)
           event_id = id
           exist_in_events = true
           break
         end
-      }
+      end
 
       if exist_in_events
         @events[event_id].hmc_add(hmc_name)
@@ -54,5 +51,4 @@ class Lssvcevents
   def count
     @events.count
   end
-
 end
