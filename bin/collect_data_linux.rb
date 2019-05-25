2539a5c1e7f2975b5d5117947eb4ca2463cc7f52
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2019-05-25 10:03:16 +0200

    Each time command is called, it is confirmed by dot ('.') on the screen (ssh will not die during very long calling commands). You can provide also data, which should be used in name of file.

--- bin/collect_data_linux.rb
+++ bin/collect_data_linux.rb
@@ -3,22 +3,32 @@ $LOAD_PATH << File.dirname(__FILE__) + '/../inc'
 
 # standard libraries
 require 'pp'
+require 'optparse'
+
+# own libraries
 require 'Framework/DataFile3'
+
+# Variables
+dateTime=Time.now.strftime("%Y%m%d_%H%M%S")
+
+
+OptionParser.new do |opts|
+  opts.on('--date YYYMMDD-HHMM', 'date of report') { |v| dateTime = v }
+end.parse!
 
 
 server=`uname -n`.chomp
-dateTime=Time.now.strftime("%Y%m%d_%H%M%S")
 filename = "/tmp/#{server}_#{dateTime}.datafile.txt"
 
 puts server
 
 file = DataFile3.new(filename)
 
-file.Write('uname -n', `uname -n`)
-file.Write('ps -ef', `ps -ef`)
-file.Write('free', `free`)
-file.Write('df -P --local', `df -P --local`)
-file.Write('df -P --local -H', `df -P --local -H`)
+file.Write('uname -n', `uname -n`, true)
+file.Write('ps -ef', `ps -ef`, true)
+file.Write('free', `free`, true)
+file.Write('df -P --local', `df -P --local`, true)
+file.Write('df -P --local -H', `df -P --local -H`, true)
 
 puts "Data have been written to file #{filename}"
 
