fd1559898dbbbcfe81834c30f3456974c6f5be50
Krzysztof Jóźwiak <krzysztof.jozwiak@linuxexpert.pl>
2020-03-13 23:41:12 +0100

    Old code removed.

--- inc/AIX/lsmpio.rb
+++ inc/AIX/lsmpio.rb
@@ -37,25 +37,6 @@ class Lsmpio
       else
         raise "wrong line: >#{line}"
       end
-
-
-      # if match = regexp.match(line)
-      #   @data['paths'] = Hash.new() unless @data.key?('paths')
-      #   @data['paths'][match[2]] = Hash.new()
-      #   @data['paths'][match[2]]['viosname'] = @viosname
-      #   @data['paths'][match[2]]['_line'] = line
-      #   @data['paths'][match[2]]['_validated'] = false
-      #   @data['paths'][match[2]]['name'] = match[1]
-      #   @data['paths'][match[2]]['path'] = match[2]
-      #   @data['paths'][match[2]]['status'] = match[3]
-      #   @data['paths'][match[2]]['path_status'] = match[4]
-      #   @data['paths'][match[2]]['parent'] = match[5]
-      #   @data['paths'][match[2]]['connection'] = match[6] + "," + match[7]
-      #   @data['paths'][match[2]]['connection_port'] = match[6]
-      #   @data['paths'][match[2]]['connection_disk'] = match[7]
-      # else
-      #   print "line:>" + line + "<\n"
-      # end
     end
 
     @disks_raw.each_pair do |key, disk_raw|
@@ -65,12 +46,5 @@ class Lsmpio
 
     @_parsed = true
   end
-
-  # def validate
-  #   @enabled_all = true
-  #   @data['paths'].keys.each do |path_id|
-  #     @enabled_all = false unless @data['paths'][path_id]['status'] == 'Enabled'
-  #   end
-  # end
 
 end
